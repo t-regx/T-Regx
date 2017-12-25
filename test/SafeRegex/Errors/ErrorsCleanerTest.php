ae85f5a386700743b0925a9c8e0336870a96a7b9
Daniel <wilkowski.kontakt@gmail.com>
2017-12-26 00:25:37 +0100

    Add ErrorsCleaner test for PHP 5.6 and earlier

--- test/SafeRegex/Errors/ErrorsCleanerTest.php
+++ test/SafeRegex/Errors/ErrorsCleanerTest.php
@@ -2,6 +2,7 @@
 namespace Test\SafeRegex\Errors;
 
 use PHPUnit\Framework\TestCase;
+use SafeRegex\Errors\Errors\OvertriggerCompileError;
 use SafeRegex\Errors\ErrorsCleaner;
 use Test\Warnings;
 
@@ -14,6 +15,8 @@ class ErrorsCleanerTest extends TestCase
      */
     public function shouldClearCompileError()
     {
+//         $this->markTestSkipped('Only for PHP 7.0 +, with error_clear_last() method');
+
         // given
         $cleaner = new ErrorsCleaner();
         $this->causeCompileWarning();
@@ -24,6 +27,26 @@ class ErrorsCleanerTest extends TestCase
         // then
         $error = error_get_last();
         $this->assertNull($error);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldClearCompileErrorPhpPre7()
+    {
+        $this->markTestSkipped('For PHP 5.6 and earlier, without error_clear_last() method');
+
+        // given
+        $cleaner = new ErrorsCleaner();
+        $this->causeCompileWarning();
+
+        // when
+        $cleaner->clear();
+
+        // then
+        $error = error_get_last();
+        $this->assertNotNull($error);
+        $this->assertEquals(OvertriggerCompileError::OVERTRIGGER_MESSAGE, $error['message']);
     }
 
     /**
