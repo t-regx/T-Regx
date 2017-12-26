e6fd91d7f678270947c7f2dd9f1dc4875f36694e
Daniel <wilkowski.kontakt@gmail.com>
2017-12-26 01:09:09 +0100

    Use GuardedExecution in \SafeRegex\preg

--- src/SafeRegex/preg.php
+++ src/SafeRegex/preg.php
@@ -2,75 +2,72 @@
 namespace SafeRegex;
 
 use SafeRegex\Constants\PregConstants;
+use SafeRegex\Guard\GuardedExecution;
 
 class preg
 {
     public static function match($pattern, $subject, array &$matches = null, $flags = 0, $offset = 0)
     {
-        $result = @preg_match($pattern, $subject, $matches, $flags, $offset);
-        self::validate('preg_match', $result);
-        return $result;
+        return GuardedExecution::invoke('preg_match', function () use ($offset, $flags, &$matches, $subject, $pattern) {
+
+            return @preg_match($pattern, $subject, $matches, $flags, $offset);
+        });
     }
 
     public static function match_all($pattern, $subject, array &$matches = null, $flags = PREG_PATTERN_ORDER, $offset = 0)
     {
-        $result = @preg_match_all($pattern, $subject, $matches, $flags, $offset);
-        self::validate('preg_match_all', $result);
-        return $result;
+        return GuardedExecution::invoke('preg_match_all', function () use ($offset, $flags, &$matches, $subject, $pattern) {
+            return @preg_match_all($pattern, $subject, $matches, $flags, $offset);
+        });
     }
 
     public static function replace($pattern, $replacement, $subject, $limit = -1, &$count = null)
     {
-        $result = @preg_replace($pattern, $replacement, $subject, $limit, $count);
-        self::validate('preg_replace', $result);
-        return $result;
+        return GuardedExecution::invoke('preg_replace', function () use ($limit, $subject, $replacement, $pattern, &$count) {
+            return @preg_replace($pattern, $replacement, $subject, $limit, $count);
+        });
     }
 
     public static function replace_callback($pattern, callable $callback, $subject, $limit = -1, &$count = null)
     {
-        $result = @preg_replace_callback($pattern, $callback, $subject, $limit, $count);
-        self::validate('preg_replace_callback', $result);
-        return $result;
+        return GuardedExecution::invoke('preg_replace_callback', function () use ($pattern, $limit, $subject, $callback, &$count) {
+            return @preg_replace_callback($pattern, $callback, $subject, $limit, $count);
+        });
     }
 
     public static function replace_callback_array($patterns_and_callbacks, $subject, $limit = -1, &$count)
     {
-        $result = @preg_replace_callback_array($patterns_and_callbacks, $subject, $limit, $count);
-        self::validate('preg_replace_callback_array', $result);
-        return $result;
+        return GuardedExecution::invoke('preg_replace_callback_array', function () use ($patterns_and_callbacks, $subject, $limit, &$count) {
+            return @preg_replace_callback_array($patterns_and_callbacks, $subject, $limit, $count);
+        });
     }
 
     public static function filter($pattern, $replacement, $subject, $limit = -1, &$count = null)
     {
-        $result = @preg_filter($pattern, $replacement, $subject, $limit, $count);
-        self::validate('preg_filter', $result);
-        return $result;
+        return GuardedExecution::invoke('preg_filter', function () use ($pattern, $replacement, $subject, $limit, &$count) {
+            return @preg_filter($pattern, $replacement, $subject, $limit, $count);
+        });
     }
 
     public static function split($pattern, $subject, $limit = -1, $flags = 0)
     {
-        $result = @preg_split($pattern, $subject, $limit, $flags);
-        self::validate('preg_split', $result);
-        return $result;
+        return GuardedExecution::invoke('preg_split', function () use ($pattern, $subject, $limit, $flags) {
+            return @preg_split($pattern, $subject, $limit, $flags);
+        });
     }
 
     public static function grep($pattern, array $input, $flags = 0)
     {
-        $result = @preg_grep($pattern, $input, $flags);
-        self::validate('preg_grep', $result);
-        return $result;
+        return GuardedExecution::invoke('preg_grep', function () use ($flags, $input, $pattern) {
+            return @preg_grep($pattern, $input, $flags);
+        });
     }
 
     public static function quote($string, $delimiter = null)
     {
-        $result = @preg_quote($string, $delimiter);
-        self::validate('preg_quote', $result);
-        return $result;
-    }
-
-    private static function validate(string $methodName, $result)
-    {
-        (new ExceptionFactory())->retrieveGlobalsAndThrow($methodName, $result);
+        return GuardedExecution::invoke('preg_quote', function () use ($delimiter, $string) {
+            return @preg_quote($string, $delimiter);
+        });
     }
 
     public static function last_error(): int
