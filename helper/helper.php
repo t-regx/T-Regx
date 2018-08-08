cdc1de7c66d0ba9236cdf39d5a125f30d0a5f4ff
Daniel Wilkowski <daniel.wilkowski@callpage.io>
2018-08-08 18:38:06 +0200

    Add $flags second argument to pattern() global method

--- helper/helper.php
+++ helper/helper.php
@@ -1,8 +1,8 @@
 <?php
 
 if (!function_exists('pattern')) {
-    function pattern(string $pattern): \CleanRegex\Pattern
+    function pattern(string $pattern, string $flags = ''): \CleanRegex\Pattern
     {
-        return new \CleanRegex\Pattern($pattern);
+        return new \CleanRegex\Pattern($pattern, $flags);
     }
 }
