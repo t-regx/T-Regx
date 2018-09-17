86593352bcfa68b1f7854f96063b434f1fcef215
Danon <wilkowski.kontakt@gmail.com>
2018-09-17 11:33:46 +0200

    Add ReplaceMatchTest (#5)

---
+++ test/Unit/CleanRegex/Match/ReplaceMatchTest.php
@@ -0,0 +1,91 @@
+<?php
+namespace Test\UnitCleanRegex\Match;
+
+use CleanRegex\Match\ReplaceMatch;
+use PHPUnit\Framework\TestCase;
+use SafeRegex\preg;
+
+class ReplaceMatchTest extends TestCase
+{
+    const INDEX_TYLER_DURDEN = 0;
+    const INDEX_MARLA_SINGER = 1;
+    const INDEX_ROBERT_PAULSON = 2;
+    const INDEX_JACK_SPARROW = 3;
+
+    const subject = 'people are always asking me if I know Tyler Durden. and suddenly I realize that all of this: ' . PHP_EOL
+    . 'the gun, the bombs, the revolution... has got something to do with a girl named Marla Singer. ' . PHP_EOL
+    . 'in death a member of project mayhem has a name. his name is Robert P***son.' . PHP_EOL
+    . PHP_EOL
+    . "when you marooned me on that god forsaken spit of land, you forgot one very important thing mate. i'm captain Jack Sparrow.";
+
+    /**
+     * @test
+     */
+    public function shouldSliceAllToLimit()
+    {
+        // given
+        $match = $this->getMatch(0, 0, 2);
+
+        // when
+        $all = $match->all();
+
+        // then
+        $this->assertEquals(['Tyler Durden', 'Marla Singer'], $all);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotSliceAllForNegativeLimit()
+    {
+        // given
+        $match = $this->getMatch(0, 0, -1);
+
+        // when
+        $all = $match->all();
+
+        // then
+        $this->assertEquals(['Tyler Durden', 'Marla Singer', 'Robert', 'Jack Sparrow'], $all);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetUnlimitedRegardlessOfLimit()
+    {
+        // given
+        $match = $this->getMatch(0, 0, 2);
+
+        // when
+        $all = $match->allUnlimited();
+
+        // then
+        $this->assertEquals(['Tyler Durden', 'Marla Singer', 'Robert', 'Jack Sparrow'], $all);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldModifyOffset()
+    {
+        // given
+        $match = $this->getMatch(0, 15, 0);
+
+        // when
+        $offset = $match->offset();
+        $modifiedOffset = $match->modifiedOffset();
+
+        // then
+        $this->assertEquals(38, $offset);
+        $this->assertEquals(53, $modifiedOffset);
+    }
+
+    private function getMatch(int $index, int $offsetModification, int $limit): ReplaceMatch
+    {
+        $pattern = '/(?<firstName>(?<initial>[A-Z])[a-z]+)(?: (?<surname>[A-Z][a-z]+))?/';
+
+        $matches = [];
+        preg::match_all($pattern, self::subject, $matches, PREG_OFFSET_CAPTURE);
+        return new ReplaceMatch(self::subject, $index, $matches, $offsetModification, $limit);
+    }
+}
