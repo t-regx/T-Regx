66333c017b107622ba5060289bffb1121943273e
Danon <wilkowski.kontakt@gmail.com>
2018-09-24 14:08:33 +0200

    Throw GroupNotMatchedException from GroupLimitFirst

--- src/TRegx/CleanRegex/Internal/GroupLimit/GroupLimitFirst.php
+++ src/TRegx/CleanRegex/Internal/GroupLimit/GroupLimitFirst.php
@@ -2,6 +2,7 @@
 namespace TRegx\CleanRegex\Internal\GroupLimit;
 
 use TRegx\CleanRegex\Exception\CleanRegex\GroupNotMatchedException;
+use TRegx\CleanRegex\Exception\CleanRegex\InternalCleanRegexException;
 use TRegx\CleanRegex\Exception\CleanRegex\NonexistentGroupException;
 use TRegx\CleanRegex\Exception\CleanRegex\SubjectNotMatchedException;
 use TRegx\CleanRegex\Internal\InternalPattern as Pattern;
@@ -31,14 +32,7 @@ class GroupLimitFirst
         if ($count === 0) {
             throw SubjectNotMatchedException::forFirst($this->subject);
         }
-        if (array_key_exists($this->nameOrIndex, $matches)) {
-            list($value, $offset) = $matches[$this->nameOrIndex];
-            if ($offset === -1) {
-                throw GroupNotMatchedException::forFirst($this->subject, $this->nameOrIndex);
-            }
-            return $value;
-        }
-        throw new NonexistentGroupException($this->nameOrIndex);
+        return $this->getGroupOrThrow($matches);
     }
 
     private function pregMatchFlags(): int
@@ -47,5 +41,49 @@ class GroupLimitFirst
             return PREG_UNMATCHED_AS_NULL;
         }
         return PREG_OFFSET_CAPTURE;
+    }
+
+    private function getGroupOrThrow(array $matches): string
+    {
+        if (array_key_exists($this->nameOrIndex, $matches)) {
+            return $this->tryGetGroup($matches[$this->nameOrIndex]);
+        }
+        throw new NonexistentGroupException($this->nameOrIndex);
+    }
+
+    /**
+     * @param array|string|null $match
+     * @return string
+     * @throws GroupNotMatchedException
+     */
+    private function tryGetGroup($match): string
+    {
+        $group = $this->getGroupFromMatch($match);
+        if ($group === null) {
+            throw GroupNotMatchedException::forFirst($this->subject, $this->nameOrIndex);
+        }
+        return $group;
+    }
+
+    /**
+     * @param array|string|null $match
+     * @return string|null
+     */
+    private function getGroupFromMatch($match): ?string
+    {
+        if ($match === null) {
+            return null;
+        }
+        if (is_string($match)) {
+            return $match;
+        }
+        if (is_array($match)) {
+            list($value, $offset) = $match;
+            if ($offset === -1) {
+                return null;
+            }
+            return $value;
+        }
+        throw new InternalCleanRegexException();
     }
 }
