c200a5993e178eed45410da8950f33d05be95e3d
Daniel Wilkowski <noreply@t-regx.com>
2019-05-10 10:47:43 +0100

    Correct ReplacePattern signature with proper interface #43

--- src/TRegx/CleanRegex/Replace/ReplacePattern.php
+++ src/TRegx/CleanRegex/Replace/ReplacePattern.php
@@ -1,8 +1,14 @@
 <?php
 namespace TRegx\CleanRegex\Replace;
 
+use TRegx\CleanRegex\Exception\CleanRegex\NotReplacedException;
 use TRegx\CleanRegex\Match\ForFirst\Optional;
 
 interface ReplacePattern extends SpecificReplacePattern, Optional
 {
+    public function orThrow(string $exceptionClassName = NotReplacedException::class): SpecificReplacePattern;
+
+    public function orReturn($default): SpecificReplacePattern;
+
+    public function orElse(callable $producer): SpecificReplacePattern;
 }
