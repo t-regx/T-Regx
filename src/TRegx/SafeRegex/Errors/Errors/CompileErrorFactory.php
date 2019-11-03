064f5a7efe31fd04ff2f7267f78598e9385b5463
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2019-11-03 15:20:16 +0100

    `CompileErrorFactory` returns `IrrelevantCompileError` for non-preg error/warning #6

--- src/TRegx/SafeRegex/Errors/Errors/CompileErrorFactory.php
+++ src/TRegx/SafeRegex/Errors/Errors/CompileErrorFactory.php
@@ -8,9 +8,20 @@ class CompileErrorFactory
     public static function getLast(): CompileError
     {
         $phpError = PhpError::getLast();
+        if ($phpError === null) {
+            return new StandardCompileError($phpError);
+        }
+        if (!CompileErrorFactory::isPregError($phpError)) {
+            return new IrrelevantCompileError();
+        }
         if (StandardCompileError::isCompatible()) {
             return new StandardCompileError($phpError);
         }
         return new OvertriggerCompileError($phpError);
+    }
+
+    private static function isPregError(PhpError $phpError): bool
+    {
+        return substr($phpError->getMessage(), 0, 5) === 'preg_';
     }
 }
---
+++ src/TRegx/SafeRegex/Errors/Errors/IrrelevantCompileError.php
@@ -0,0 +1,24 @@
+<?php
+namespace TRegx\SafeRegex\Errors\Errors;
+
+use TRegx\CleanRegex\Exception\CleanRegex\InternalCleanRegexException;
+use TRegx\SafeRegex\Exception\SafeRegexException;
+
+class IrrelevantCompileError implements CompileError
+{
+    public function occurred(): bool
+    {
+        return false; // It really occurred, but let T-Regx think nothing happened
+    }
+
+    public function clear(): void
+    {
+    }
+
+    public function getSafeRegexpException(string $methodName): SafeRegexException
+    {
+        // @codeCoverageIgnoreStart
+        throw new InternalCleanRegexException();
+        // @codeCoverageIgnoreEnd
+    }
+}
