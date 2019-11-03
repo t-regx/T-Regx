310325378658290ad6462e0d34f1e27f998865f4
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2019-11-03 02:10:59 +0100

    Split `PatternBuilderTest` into multiple files

--- test/Feature/TRegx/CleanRegex/PatternBuilder/PatternBuilderTest.php
+++ test/Feature/TRegx/CleanRegex/PatternBuilder/PatternBuilderTest.php
@@ -10,59 +10,9 @@ class PatternBuilderTest extends TestCase
     /**
      * @test
      */
-    public function shouldBuild_prepared()
-    {
-        // given
-        $pattern = PatternBuilder::builder()->prepare(['You/her, (are|is) ', ['real? (or are you not real?)'], ' (you|her)']);
-
-        // when
-        $pattern = $pattern->delimiter();
-
-        // then
-        $this->assertEquals('#You/her, (are|is) real\? \(or are you not real\?\) (you|her)#', $pattern);
-    }
-
-    /**
-     * @test
-     */
-    public function shouldBuild_bind()
-    {
-        // given
-        $pattern = PatternBuilder::builder()->bind('You/her, (are|is) @question (you|her)', [
-            'question' => 'real? (or are you not real?)'
-        ]);
-
-        // when
-        $pattern = $pattern->delimiter();
-
-        // then
-        $this->assertEquals('#You/her, (are|is) real\? \(or are you not real\?\) (you|her)#', $pattern);
-    }
-
-    /**
-     * @test
-     */
-    public function shouldBuild_inject()
-    {
-        // given
-        $pattern = PatternBuilder::builder()->inject('You/her, (are|is) @ (you|her)', [
-            'real? (or are you not real?)'
-        ]);
-
-        // when
-        $pattern = $pattern->delimiter();
-
-        // then
-        $this->assertEquals('#You/her, (are|is) real\? \(or are you not real\?\) (you|her)#', $pattern);
-    }
-
-    /**
-     * @test
-     */
     public function shouldBuild_compose()
     {
         // given
-        $name = 'Frodo';
         $pattern = PatternBuilder::compose([
             pattern('^Fro'),
             Pattern::of('rod'),
@@ -70,7 +20,7 @@ class PatternBuilderTest extends TestCase
         ]);
 
         // when
-        $matches = $pattern->allMatch($name);
+        $matches = $pattern->allMatch('Frodo');
 
         // then
         $this->assertTrue($matches);
---
+++ test/Feature/TRegx/CleanRegex/PatternBuilder/builder/PatternBuilderTest.php
@@ -0,0 +1,58 @@
+<?php
+namespace Test\Feature\TRegx\CleanRegex\PatternBuilder\builder;
+
+use PHPUnit\Framework\TestCase;
+use TRegx\CleanRegex\Pattern;
+use TRegx\CleanRegex\PatternBuilder;
+
+class PatternBuilderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldBuild_prepared()
+    {
+        // given
+        $pattern = PatternBuilder::builder()->prepare(['You/her, (are|is) ', ['real? (or are you not real?)'], ' (you|her)']);
+
+        // when
+        $pattern = $pattern->delimiter();
+
+        // then
+        $this->assertEquals('#You/her, (are|is) real\? \(or are you not real\?\) (you|her)#', $pattern);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBuild_bind()
+    {
+        // given
+        $pattern = PatternBuilder::builder()->bind('You/her, (are|is) @question (you|her)', [
+            'question' => 'real? (or are you not real?)'
+        ]);
+
+        // when
+        $pattern = $pattern->delimiter();
+
+        // then
+        $this->assertEquals('#You/her, (are|is) real\? \(or are you not real\?\) (you|her)#', $pattern);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBuild_inject()
+    {
+        // given
+        $pattern = PatternBuilder::builder()->inject('You/her, (are|is) @ (you|her)', [
+            'real? (or are you not real?)'
+        ]);
+
+        // when
+        $pattern = $pattern->delimiter();
+
+        // then
+        $this->assertEquals('#You/her, (are|is) real\? \(or are you not real\?\) (you|her)#', $pattern);
+    }
+}
