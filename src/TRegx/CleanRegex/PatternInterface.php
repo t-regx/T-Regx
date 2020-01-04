ddb14e1553a672364511b6de4edf17e92e450db1
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-01-04 17:07:23 +0100

    Created PatternInterface interface #62

---
+++ src/TRegx/CleanRegex/PatternInterface.php
@@ -0,0 +1,60 @@
+<?php
+namespace TRegx\CleanRegex;
+
+use TRegx\CleanRegex\ForArray\ForArrayPattern;
+use TRegx\CleanRegex\Match\MatchPattern;
+use TRegx\CleanRegex\Remove\RemoveLimit;
+use TRegx\CleanRegex\Replace\ReplaceLimit;
+
+interface PatternInterface
+{
+    /**
+     * {@documentary:test}
+     */
+    public function test(string $subject): bool;
+
+    /**
+     * {@documentary:test}
+     */
+    public function fails(string $subject): bool;
+
+    /**
+     * {@documentary:test}
+     */
+    public function match(string $subject): MatchPattern;
+
+    /**
+     * {@documentary:test}
+     */
+    public function replace(string $subject): ReplaceLimit;
+
+    /**
+     * {@documentary:test}
+     */
+    public function remove(string $subject): RemoveLimit;
+
+    /**
+     * {@documentary:test}
+     */
+    public function forArray(array $haystack): ForArrayPattern;
+
+    /**
+     * {@documentary:test}
+     */
+    public function split(string $subject): array;
+
+    /**
+     * {@documentary:test}
+     */
+    public function count(string $subject): int;
+
+    /**
+     * {@documentary:test}
+     */
+    public function valid(): bool;
+
+    /**
+     * {@documentary:test}
+     */
+    public function delimiter(): string;
+}
