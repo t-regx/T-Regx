5f4fe876a601c61649c65200499f1156f323fbc1
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-03-04 22:03:06 +0100

    GroupLimitMocks mocks Base, not each factory separately (tied tests with logic, intentionally)

--- test/Integration/TRegx/CleanRegex/Match/GroupLimitMocks.php
+++ test/Integration/TRegx/CleanRegex/Match/GroupLimitMocks.php
@@ -3,11 +3,7 @@ namespace Test\Integration\TRegx\CleanRegex\Match;
 
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
-use TRegx\CleanRegex\Internal\GroupLimit\GroupLimitAll;
-use TRegx\CleanRegex\Internal\GroupLimit\GroupLimitFirst;
-use TRegx\CleanRegex\Internal\InternalPattern;
-use TRegx\CleanRegex\Internal\Match\Base\ApiBase;
-use TRegx\CleanRegex\Internal\Match\UserData;
+use TRegx\CleanRegex\Internal\Match\Base\Base;
 use TRegx\CleanRegex\Internal\Model\Match\RawMatchOffset;
 use TRegx\CleanRegex\Internal\Model\Matches\RawMatchesOffset;
 use TRegx\CleanRegex\Internal\OffsetLimit\MatchOffsetLimitFactory;
@@ -17,26 +13,11 @@ class GroupLimitMocks extends TestCase // this is a dirty hack, only to use prot
 {
     public static function mockGroupLimit(TestCase $test, array $allValues, string $firstValue = ''): GroupLimit
     {
-        $base = new ApiBase(InternalPattern::pcre('//'), 'unused', new UserData());
-        return new GroupLimit(
-            self::all($test, $allValues),
-            self::first($test, $firstValue),
-            new MatchOffsetLimitFactory($base, 0, false), $base, 0);
-    }
+        /** @var Base|MockObject $base */
+        $base = $test->createMock(Base::class);
+        $base->method('matchOffset')->willReturn(new RawMatchOffset([0 => [$firstValue, 0]]));
+        $base->method('matchAllOffsets')->willReturn(new RawMatchesOffset([0 => $allValues]));
 
-    private static function first(TestCase $test, string $firstValue): GroupLimitFirst
-    {
-        /** @var GroupLimitFirst|MockObject $first */
-        $first = $test->createMock(GroupLimitFirst::class);
-        $first->method('getFirstForGroup')->willReturn(new RawMatchOffset([0 => [$firstValue, 0]]));
-        return $first;
-    }
-
-    private static function all(TestCase $test, array $allValues): GroupLimitAll
-    {
-        /** @var GroupLimitAll|MockObject $all */
-        $all = $test->createMock(GroupLimitAll::class);
-        $all->method('getAllForGroup')->willReturn(new RawMatchesOffset([0 => $allValues]));
-        return $all;
+        return new GroupLimit(new MatchOffsetLimitFactory($base, 0, false), $base, 0);
     }
 }
