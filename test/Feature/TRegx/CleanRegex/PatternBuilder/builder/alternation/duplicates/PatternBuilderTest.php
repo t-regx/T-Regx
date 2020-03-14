dc0ea387280bcfeb79a5de5d128ce46a56a2af85
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-03-14 14:19:19 +0100

    PatternBuilder passes flags to PrepareFacade #61

---
+++ test/Feature/TRegx/CleanRegex/PatternBuilder/builder/alternation/duplicates/PatternBuilderTest.php
@@ -0,0 +1,76 @@
+<?php
+namespace Test\Feature\TRegx\CleanRegex\PatternBuilder\builder\alternation\duplicates;
+
+use PHPUnit\Framework\TestCase;
+use TRegx\CleanRegex\Pattern;
+
+class PatternTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider flagsAndAlternationResults
+     * @param string $flags
+     * @param string $expected
+     */
+    public function shouldBuild_prepared(string $flags, string $expected)
+    {
+        // given
+        $pattern = Pattern::prepare(['Foo', [$this->alternation()]], $flags);
+
+        // when
+        $patter = $pattern->delimiter();
+
+        // then
+        $this->assertEquals("/Foo(?:$expected)/$flags", $patter);
+    }
+
+    /**
+     * @test
+     * @dataProvider flagsAndAlternationResults
+     * @param string $flags
+     * @param string $expected
+     */
+    public function shouldBuild_bind(string $flags, string $expected)
+    {
+        // given
+        $pattern = Pattern::bind('Foo@bar', ['bar' => $this->alternation()], $flags);
+
+        // when
+        $patter = $pattern->delimiter();
+
+        // then
+        $this->assertEquals("/Foo(?:$expected)/$flags", $patter);
+    }
+
+    /**
+     * @test
+     * @dataProvider flagsAndAlternationResults
+     * @param string $flags
+     * @param string $expected
+     */
+    public function shouldBuild_inject(string $flags, string $expected)
+    {
+        // given
+        $pattern = Pattern::inject('Foo@', [$this->alternation()], $flags);
+
+        // when
+        $patter = $pattern->delimiter();
+
+        // then
+        $this->assertEquals("/Foo(?:$expected)/$flags", $patter);
+    }
+
+    public function flagsAndAlternationResults(): array
+    {
+        return [
+            'no flags'  => ['', 'Foo|foo|łóżko|ŁÓŻKO'],
+            'flags: i'  => ['i', 'Foo|łóżko|ŁÓŻKO'],
+            'flags: ui' => ['ui', 'Foo|łóżko']
+        ];
+    }
+
+    private function alternation(): array
+    {
+        return ['Foo', 'foo', 'łóżko', 'ŁÓŻKO'];
+    }
+}
