dc0440ca9e5ad957ace0cc59a3046d070ef1eb28
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-03-15 19:17:59 +0100

    Add AbstractMatchPatternTest.test_asInt_first()

--- test/Integration/TRegx/CleanRegex/Match/AbstractMatchPatternTest.php
+++ test/Integration/TRegx/CleanRegex/Match/AbstractMatchPatternTest.php
@@ -11,7 +11,7 @@ class AbstractMatchPatternTest extends TestCase
     /**
      * @test
      */
-    public function test_asInt()
+    public function test_asInt_all()
     {
         // given
         $pattern = new MatchPattern(InternalPattern::pcre('/\d+/'), 'Foo 1 Bar 34 Lorem 42 Ipsum');
@@ -21,6 +21,21 @@ class AbstractMatchPatternTest extends TestCase
 
         // then
         $this->assertSame([1, 34, 42], $integers);
+    }
+
+    /**
+     * @test
+     */
+    public function test_asInt_first()
+    {
+        // given
+        $pattern = new MatchPattern(InternalPattern::pcre('/\d+/'), 'Foo 1 Bar 34 Lorem 42 Ipsum');
+
+        // when
+        $integers = $pattern->asInt()->first();
+
+        // then
+        $this->assertEquals(1, $integers);
     }
 
     /**
