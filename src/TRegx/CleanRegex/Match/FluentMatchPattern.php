a5310a46f3c0cdc7a06730884ecb9fc0af7e90cb
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-03-25 22:31:20 +0100

    Change implementation of FluentMatchPattern from `array` to `Switcher` #74

--- src/TRegx/CleanRegex/Match/FluentMatchPattern.php
+++ src/TRegx/CleanRegex/Match/FluentMatchPattern.php
@@ -10,9 +10,12 @@ use TRegx\CleanRegex\Exception\InvalidReturnValueException;
 use TRegx\CleanRegex\Exception\NoSuchElementFluentException;
 use TRegx\CleanRegex\Internal\Exception\Messages\NoFirstElementFluentMessage;
 use TRegx\CleanRegex\Internal\Exception\Messages\NoSuchElementFluentMessage;
+use TRegx\CleanRegex\Internal\Exception\NoFirstSwitcherException;
 use TRegx\CleanRegex\Internal\Factory\NotMatchedFluentOptionalWorker;
 use TRegx\CleanRegex\Internal\Integer;
 use TRegx\CleanRegex\Internal\Match\FlatMapper;
+use TRegx\CleanRegex\Internal\Match\Switcher\ArraySwitcher;
+use TRegx\CleanRegex\Internal\Match\Switcher\Switcher;
 use TRegx\CleanRegex\Match\Details\Group\MatchGroup;
 use TRegx\CleanRegex\Match\Details\Match;
 use TRegx\CleanRegex\Match\FindFirst\MatchedOptional;
@@ -20,20 +23,20 @@ use TRegx\CleanRegex\Match\FindFirst\Optional;
 
 class FluentMatchPattern implements MatchPatternInterface
 {
-    /** @var array */
-    private $elements;
+    /** @var Switcher */
+    private $switcher;
     /** @var NotMatchedFluentOptionalWorker */
     private $firstWorker;
 
-    public function __construct(array $elements, NotMatchedFluentOptionalWorker $firstWorker)
+    public function __construct(array $switcher, NotMatchedFluentOptionalWorker $firstWorker)
     {
-        $this->elements = $elements;
+        $this->switcher = new ArraySwitcher($switcher);
         $this->firstWorker = $firstWorker;
     }
 
     public function all(): array
     {
-        return $this->elements;
+        return $this->switcher->all();
     }
 
     public function only(int $limit): array
@@ -41,7 +44,7 @@ class FluentMatchPattern implements MatchPatternInterface
         if ($limit < 0) {
             throw new InvalidArgumentException("Negative limit: $limit");
         }
-        return \array_slice($this->elements, 0, $limit);
+        return \array_slice($this->switcher->all(), 0, $limit);
     }
 
     /**
@@ -51,19 +54,21 @@ class FluentMatchPattern implements MatchPatternInterface
      */
     public function first(callable $consumer = null)
     {
-        if (empty($this->elements)) {
+        try {
+            $firstElement = $this->switcher->first();
+            return $consumer ? $consumer($firstElement) : $firstElement;
+        } catch (NoFirstSwitcherException $exception) {
             throw NoSuchElementFluentException::withMessage(new NoFirstElementFluentMessage());
         }
-        $firstElement = \reset($this->elements);
-        return $consumer ? $consumer($firstElement) : $firstElement;
     }
 
     public function findFirst(callable $consumer): Optional
     {
-        if (empty($this->elements)) {
+        try {
+            return new MatchedOptional($consumer($this->switcher->first()));
+        } catch (NoFirstSwitcherException $exception) {
             return new NotMatchedFluentOptional($this->firstWorker);
         }
-        return new MatchedOptional($consumer(\reset($this->elements)));
     }
 
     public function nth(int $index)
@@ -76,7 +81,7 @@ class FluentMatchPattern implements MatchPatternInterface
         if ($index < 0) {
             throw new InvalidArgumentException("Negative index: $index");
         }
-        $elements = \array_values($this->elements);
+        $elements = \array_values($this->switcher->all());
         if (\array_key_exists($index, $elements)) {
             return new MatchedOptional($elements[$index]);
         }
@@ -85,49 +90,49 @@ class FluentMatchPattern implements MatchPatternInterface
 
     public function forEach(callable $consumer): void
     {
-        foreach ($this->elements as $key => $value) {
+        foreach ($this->switcher->all() as $key => $value) {
             $consumer($value, $key);
         }
     }
 
     public function count(): int
     {
-        return \count($this->elements);
+        return \count($this->switcher->all());
     }
 
     public function iterator(): Iterator
     {
-        return new ArrayIterator($this->elements);
+        return new ArrayIterator($this->switcher->all());
     }
 
     public function map(callable $mapper): FluentMatchPattern
     {
-        return $this->next(\array_map($mapper, $this->elements));
+        return $this->next(\array_map($mapper, $this->switcher->all()));
     }
 
     public function flatMap(callable $mapper): FluentMatchPattern
     {
-        return $this->next((new FlatMapper($this->elements, $mapper))->get());
+        return $this->next((new FlatMapper($this->switcher->all(), $mapper))->get());
     }
 
     public function distinct(): FluentMatchPattern
     {
-        return $this->next(\array_values(\array_unique($this->elements)));
+        return $this->next(\array_values(\array_unique($this->switcher->all())));
     }
 
     public function filter(callable $predicate): FluentMatchPattern
     {
-        return $this->next(\array_values(\array_filter($this->elements, $predicate)));
+        return $this->next(\array_values(\array_filter($this->switcher->all(), $predicate)));
     }
 
     public function values(): FluentMatchPattern
     {
-        return $this->next(\array_values($this->elements));
+        return $this->next(\array_values($this->switcher->all()));
     }
 
     public function keys(): FluentMatchPattern
     {
-        return $this->next(\array_keys($this->elements));
+        return $this->next(\array_keys($this->switcher->all()));
     }
 
     public function asInt(): FluentMatchPattern
@@ -152,7 +157,7 @@ class FluentMatchPattern implements MatchPatternInterface
     public function groupByCallback(callable $groupMapper): FluentMatchPattern
     {
         $map = [];
-        foreach ($this->elements as $element) {
+        foreach ($this->switcher->all() as $element) {
             $key = $groupMapper($element);
             if (\is_int($key) || \is_string($key)) {
                 $map[$key][] = $element;
