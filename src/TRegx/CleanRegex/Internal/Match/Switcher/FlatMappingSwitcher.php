dc03e2cb933d46d3367f3ba72842988668998501
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-03-26 22:46:27 +0100

    Add FlatMappingSwitcher #74

---
+++ src/TRegx/CleanRegex/Internal/Match/Switcher/FlatMappingSwitcher.php
@@ -0,0 +1,28 @@
+<?php
+namespace TRegx\CleanRegex\Internal\Match\Switcher;
+
+use TRegx\CleanRegex\Internal\Match\FlatMapper;
+
+class FlatMappingSwitcher implements Switcher
+{
+    /** @var array */
+    private $switcher;
+    /** @var callable */
+    private $mapper;
+
+    public function __construct(Switcher $switcher, callable $mapper)
+    {
+        $this->switcher = $switcher;
+        $this->mapper = $mapper;
+    }
+
+    public function all(): array
+    {
+        return (new FlatMapper($this->switcher->all(), $this->mapper))->get();
+    }
+
+    public function first()
+    {
+        return (new FlatMapper([$this->switcher->first()], $this->mapper))->get();
+    }
+}
---
+++ test/Unit/TRegx/CleanRegex/Internal/Match/Switcher/FlatMappingSwitcherTest.php
@@ -0,0 +1,111 @@
+<?php
+namespace TRegx\CleanRegex\Internal\Match\Switcher;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use TRegx\CleanRegex\Exception\InvalidReturnValueException;
+use TRegx\CleanRegex\Internal\Exception\NoFirstSwitcherException;
+
+class FlatMappingSwitcherTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldGetAll()
+    {
+        // given
+        $switcher = new FlatMappingSwitcher($this->mock('all', 'willReturn', ['One', 'Two', 'Three']), 'str_split');
+
+        // when
+        $all = $switcher->all();
+
+        // then
+        $this->assertSame(['O', 'n', 'e', 'T', 'w', 'o', 'T', 'h', 'r', 'e', 'e'], $all);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetFirst()
+    {
+        // given
+        $switcher = new FlatMappingSwitcher($this->mock('first', 'willReturn', 'One'), 'str_split');
+
+        // when
+        $first = $switcher->first();
+
+        // then
+        $this->assertSame(['O', 'n', 'e'], $first);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldFirstThrow_forNoFirstElement()
+    {
+        // given
+        $switcher = new FlatMappingSwitcher($this->mock('first', 'willThrowException', new NoFirstSwitcherException()), 'strlen');
+
+        // then
+        $this->expectException(NoFirstSwitcherException::class);
+
+        // when
+        $switcher->first();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldReturn_forEmptyArray()
+    {
+        // given
+        $switcher = new FlatMappingSwitcher($this->mock('first', 'willReturn', []), function ($a) {
+            return $a;
+        });
+
+        // when
+        $first = $switcher->first();
+
+        // then
+        $this->assertSame([], $first);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldFirstThrow_invalidReturnType()
+    {
+        // given
+        $switcher = new FlatMappingSwitcher($this->mock('first', 'willReturn', 'Foo'), 'strlen');
+
+        // then
+        $this->expectException(InvalidReturnValueException::class);
+
+        // when
+        $switcher->first();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldAllThrow_invalidReturnType()
+    {
+        // given
+        $switcher = new FlatMappingSwitcher($this->mock('all', 'willReturn', ['Foo']), 'strlen');
+
+        // then
+        $this->expectException(InvalidReturnValueException::class);
+
+        // when
+        $switcher->all();
+    }
+
+    private function mock(string $methodName, string $setter, $value): Switcher
+    {
+        /** @var Switcher|MockObject $switcher */
+        $switcher = $this->createMock(Switcher::class);
+        $switcher->expects($this->once())->method($methodName)->$setter($value);
+        $switcher->expects($this->never())->method($this->logicalNot($this->matches($methodName)));
+        return $switcher;
+    }
+}
