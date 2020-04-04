7a411de87b6eab5b8e95ed1c88e70e898e1bbfda
Daniel Wilkowski <daniel.wilkowski@ig.com>
2020-04-04 19:58:57 +0200

    Add unit test for LazyRawWithGroupsTest, that checks that `preg_match_all()` can avoid catastrophic backtracking

---
+++ test/Unit/TRegx/CleanRegex/Match/Details/LazyRawWithGroupsTest.php
@@ -0,0 +1,47 @@
+<?php
+namespace Test\Unit\TRegx\CleanRegex\Match\Details;
+
+use PHPUnit\Framework\TestCase;
+use TRegx\CleanRegex\Internal\InternalPattern;
+use TRegx\CleanRegex\Internal\Match\Base\ApiBase;
+use TRegx\CleanRegex\Internal\Match\Base\Base;
+use TRegx\CleanRegex\Internal\Match\UserData;
+use TRegx\CleanRegex\Match\Details\LazyRawWithGroups;
+
+class LazyRawWithGroupsTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldNotCallAll()
+    {
+        // given
+        $rawWithGroups = new LazyRawWithGroups($this->base());
+
+        // when
+        $keys = $rawWithGroups->getGroupKeys();
+
+        // then
+        $this->assertEquals([0, 1, 2], $keys);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCheckGroups()
+    {
+        // given
+        $rawWithGroups = new LazyRawWithGroups($this->base());
+
+        // when
+        $hasGroup = $rawWithGroups->hasGroup(1);
+
+        // then
+        $this->assertTrue($hasGroup);
+    }
+
+    private function base(): Base
+    {
+        return new ApiBase(InternalPattern::standard("((\w+\w+)+3)"), '  123 aaaaaaaaaaaaaaaaaaaa 3', new UserData());
+    }
+}
