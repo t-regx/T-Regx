bab1ecc6531426781b8d7ddb50ad18ae55e6e551
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-04-25 01:14:18 +0200

    Update DelimitererTest

--- test/Unit/TRegx/CleanRegex/Internal/Delimiter/DelimitererTest.php
+++ test/Unit/TRegx/CleanRegex/Internal/Delimiter/DelimitererTest.php
@@ -60,11 +60,16 @@ class DelimitererTest extends TestCase
     {
         // given
         $delimiterer = new Delimiterer(new IdentityStrategy());
+        $pattern = 's~i/e#++m%a!@*`_-;=,' . chr(1);
+
+        $message = "Unfortunately, CleanRegex couldn't find any indistinct delimiter to match your pattern \"$pattern\". " .
+            'Please specify the delimiter explicitly, and escape the delimiter character inside your pattern.';
 
         // then
         $this->expectException(ExplicitDelimiterRequiredException::class);
+        $this->expectExceptionMessage($message);
 
         // when
-        $delimiterer->delimiter('s~i/e#++m%a!@*`_-;=,' . chr(1));
+        $delimiterer->delimiter($pattern);
     }
 }
