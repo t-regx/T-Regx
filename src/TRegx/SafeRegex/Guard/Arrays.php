ea57ba3160f5c7f0d3711df9749496c9cdff9c9f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-05-03 22:04:08 +0200

    Remove Arrays.equal()

--- src/TRegx/SafeRegex/Guard/Arrays.php
+++ src/TRegx/SafeRegex/Guard/Arrays.php
@@ -1,16 +1,10 @@
 <?php
 namespace TRegx\SafeRegex\Guard;
 
-use function array_diff_key;
 use function array_merge;
 
 class Arrays
 {
-    public static function equal(array $array1, array $array2): bool
-    {
-        return !array_diff_key($array1, $array2) && !array_diff_key($array2, $array1);
-    }
-
     public static function flatten(array $array): array
     {
         return array_merge(...$array);
