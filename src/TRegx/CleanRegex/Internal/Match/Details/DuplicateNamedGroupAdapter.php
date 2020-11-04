3f2783851e874c60a216450a0dea4eb16972c323
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-11-04 20:16:11 +0100

    Add DuplicateNamedGroupAdapter

---
+++ src/TRegx/CleanRegex/Internal/Match/Details/DuplicateNamedGroupAdapter.php
@@ -0,0 +1,104 @@
+<?php
+namespace TRegx\CleanRegex\Internal\Match\Details;
+
+use TRegx\CleanRegex\Match\Details\DuplicateNamedGroup;
+use TRegx\CleanRegex\Match\Details\Group\MatchGroup;
+
+class DuplicateNamedGroupAdapter implements DuplicateNamedGroup
+{
+    /** @var string */
+    private $name;
+    /** @var MatchGroup */
+    private $group;
+
+    public function __construct(string $name, MatchGroup $group)
+    {
+        $this->name = $name;
+        $this->group = $group;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function text(): string
+    {
+        return $this->group->text();
+    }
+
+    public function textLength(): int
+    {
+        return $this->group->textLength();
+    }
+
+    public function toInt(): int
+    {
+        return $this->group->toInt();
+    }
+
+    public function isInt(): bool
+    {
+        return $this->group->isInt();
+    }
+
+    public function matched(): bool
+    {
+        return $this->group->matched();
+    }
+
+    public function equals(string $expected): bool
+    {
+        return $this->group->equals($expected);
+    }
+
+    public function usedIdentifier()
+    {
+        return $this->group->usedIdentifier();
+    }
+
+    public function offset(): int
+    {
+        return $this->group->offset();
+    }
+
+    public function tail(): int
+    {
+        return $this->group->tail();
+    }
+
+    public function byteOffset(): int
+    {
+        return $this->group->byteOffset();
+    }
+
+    public function byteTail(): int
+    {
+        return $this->group->byteTail();
+    }
+
+    public function replace(string $replacement): string
+    {
+        return $this->group->replace($replacement);
+    }
+
+    public function all(): array
+    {
+        return $this->group->all();
+    }
+
+    public function orThrow(string $exceptionClassName = null)
+    {
+        return $this->group->orThrow($exceptionClassName);
+    }
+
+    public function orReturn($substitute)
+    {
+        return $this->group->orReturn($substitute);
+    }
+
+    public function orElse(callable $substituteProducer)
+    {
+        return $this->group->orElse($substituteProducer);
+    }
+}
---
+++ test/Unit/TRegx/CleanRegex/Internal/Match/Details/DuplicateNamedGroupAdapterTest.php
@@ -0,0 +1,81 @@
+<?php
+namespace Test\Unit\TRegx\CleanRegex\Internal\Match\Details;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use TRegx\CleanRegex\Internal\Match\Details\DuplicateNamedGroupAdapter;
+use TRegx\CleanRegex\Match\Details\Group\MatchGroup;
+
+class DuplicateNamedGroupAdapterTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldGetName(): void
+    {
+        // given
+        $adapter = new DuplicateNamedGroupAdapter('foo', $this->matchGroup('name', [], 'bar', 0));
+
+        // when
+        $name = $adapter->name();
+
+        // then
+        $this->assertEquals('foo', $name);
+    }
+
+    /**
+     * @test
+     * @dataProvider methods
+     * @param string $method
+     * @param array $arguments
+     * @param $value
+     */
+    public function shouldDelegate(string $method, array $arguments, $value): void
+    {
+        // given
+        $adapter = new DuplicateNamedGroupAdapter('', $this->matchGroup($method, $arguments, $value));
+
+        // when
+        $result = $adapter->$method(...$arguments);
+
+        // then
+        $this->assertEquals($value, $result);
+    }
+
+    public function methods(): array
+    {
+        return [
+            'text'           => ['text', [], 'value'],
+            'textLength'     => ['textLength', [], 14],
+            'toInt'          => ['toInt', [], 14],
+            'isInt'          => ['isInt', [], true],
+            'equals'         => ['equals', ['arg'], true],
+            'replace'        => ['replace', ['arg'], 'value'],
+            'matched'        => ['matched', [], true],
+            'usedIdentifier' => ['usedIdentifier', [], 12],
+            'offset'         => ['offset', [], 12],
+            'tail'           => ['tail', [], 12],
+            'byteOffset'     => ['byteOffset', [], 12],
+            'byteTail'       => ['byteTail', [], 12],
+            'all'            => ['all', [], ['a', 'b']],
+            'orThrow []'     => ['orThrow', [], 'substitute'],
+            'orThrow [arg]'  => ['orThrow', [\stdClass::class], 'substitute'],
+            'orReturn'       => ['orReturn', ['substitute'], 'substitute'],
+            'orElse'         => ['orElse', ['strtoupper'], 'substitute'],
+        ];
+    }
+
+    private function matchGroup(string $method, $arguments, $result, int $count = 1): MatchGroup
+    {
+        /** @var MockObject|MatchGroup $group */
+        $group = $this->createMock(MatchGroup::class);
+
+        $group
+            ->expects($this->exactly($count))
+            ->method($method)
+            ->with(...$arguments)
+            ->will($this->returnValue($result));
+
+        return $group;
+    }
+}
