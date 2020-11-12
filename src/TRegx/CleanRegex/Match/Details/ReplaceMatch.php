fb65652aee119e4e1202b39a3eabee86a4e4fc3a
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-11-12 20:37:28 +0100

    Add explaining comments to Match and ReplaceMatch

--- src/TRegx/CleanRegex/Match/Details/ReplaceMatch.php
+++ src/TRegx/CleanRegex/Match/Details/ReplaceMatch.php
@@ -2,6 +2,9 @@
 namespace TRegx\CleanRegex\Match\Details;
 
 /**
+ * In PHP 8, "match" will become a keyword and thus will be unfit
+ * for class names and namespaces.
+ *
  * @deprecated Use {@see \TRegx\CleanRegex\Match\Details\ReplaceDetail} instead
  */
 interface ReplaceMatch extends ReplaceDetail, Match
