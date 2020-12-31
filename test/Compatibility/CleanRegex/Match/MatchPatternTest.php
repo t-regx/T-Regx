76c4b9783f50f7b5b74a0f600d817efcd03e7c14
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2020-12-31 15:22:52 +0100

    Add missing MatchPatternTest.shouldCallAsMatchGroup()

--- test/Compatibility/CleanRegex/Match/MatchPatternTest.php
+++ test/Compatibility/CleanRegex/Match/MatchPatternTest.php
@@ -4,6 +4,9 @@ namespace Test\Compatibility\TRegx\CleanRegex\Match;
 use PHPUnit\Framework\TestCase;
 use TRegx\CleanRegex\Match\Details\Detail;
 use TRegx\CleanRegex\Match\Details\DetailImpl;
+use TRegx\CleanRegex\Match\Details\Group\DetailGroup;
+use TRegx\CleanRegex\Match\Details\Group\MatchedGroup;
+use TRegx\CleanRegex\Match\Details\Group\MatchGroup;
 use TRegx\CleanRegex\Match\Details\Match;
 
 class MatchPatternTest extends TestCase
@@ -18,5 +21,19 @@ class MatchPatternTest extends TestCase
             $this->assertInstanceOf(DetailImpl::class, $match);
             $this->assertInstanceOf(Detail::class, $match);
         });
+    }
+
+    /**
+     * @test
+     */
+    public function shouldCallAsMatchGroup()
+    {
+        // when
+        pattern('([A-Za-z]){4}\.')->match('What do you need? - Guns.')
+            ->group(1)
+            ->first(function (MatchGroup $match) {
+                $this->assertInstanceOf(MatchedGroup::class, $match);
+                $this->assertInstanceOf(DetailGroup::class, $match);
+            });
     }
 }
