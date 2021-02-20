f656bbc310c24896390db6941f0d10cb5faefc85
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-02-20 11:50:20 +0100

    Update ReplaceDetailTest test

--- test/Interaction/CleanRegex/Match/Details/ReplaceDetailTest.php
+++ test/Interaction/CleanRegex/Match/Details/ReplaceDetailTest.php
@@ -11,86 +11,72 @@ use TRegx\CleanRegex\Internal\Subject;
 use TRegx\CleanRegex\Match\Details\MatchDetail;
 use TRegx\CleanRegex\Match\Details\ReplaceDetail;
 use TRegx\CleanRegex\Match\Details\ReplaceDetailImpl;
-use TRegx\SafeRegex\preg;
 
 class ReplaceDetailTest extends TestCase
 {
-    private const subject = "people are always asking me if I know Tyler Durden. and suddenly I realize that all of this: 
-the gun, the bombs, the revolution... has got something to do with a girl named Marla Singer. 
-in death a member of project mayhem has a name. his name is Robert P***son.
-
-when you marooned me on that god forsaken spit of land, you forgot one very important thing mate. i'm captain Jack Sparrow.";
-
     /**
      * @test
      */
-    public function shouldSliceAllToLimit()
+    public function shouldGet_all()
     {
         // given
-        $detail = $this->detail(0);
+        $matches = [[['Tyler Durden', 0], ['Marla Singer', 1]],];
+        $detail = $this->detail('', $matches, -1);
 
         // when
         $all = $detail->all();
 
         // then
-        $this->assertSame(['Tyler Durden', 'Marla Singer', 'Robert', 'Jack Sparrow'], $all);
+        $this->assertSame(['Tyler Durden', 'Marla Singer'], $all);
     }
 
     /**
      * @test
      */
-    public function shouldNotSliceAllForNegativeLimit()
+    public function shouldGet_modifiedOffset()
     {
         // given
-        $detail = $this->detail(0);
-
-        // when
-        $all = $detail->all();
-
-        // then
-        $this->assertSame(['Tyler Durden', 'Marla Singer', 'Robert', 'Jack Sparrow'], $all);
-    }
-
-    /**
-     * @test
-     */
-    public function shouldModifyOffset()
-    {
-        // given
-        $detail = $this->detail(15);
+        $detail = $this->detail('€€ Bar', [[['', 3]]], 3);
 
         // when
         $offset = $detail->offset();
+        $modifiedOffset = $detail->modifiedOffset();
+
+        // then
+        $this->assertSame(1, $offset);
+        $this->assertSame(2, $modifiedOffset);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGet_byteModifiedOffset()
+    {
+        // given
+        $detail = $this->detail('€€ Bar', [[['', 3]]], 3);
+
+        // when
+        $offset = $detail->byteOffset();
         $modifiedOffset = $detail->byteModifiedOffset();
 
         // then
-        $this->assertSame(38, $offset);
-        $this->assertSame(53, $modifiedOffset);
+        $this->assertSame(3, $offset);
+        $this->assertSame(6, $modifiedOffset);
     }
 
-    private function detail(int $offsetModification): ReplaceDetail
+    private function detail(string $subject, array $matches, int $offsetModification): ReplaceDetail
     {
-        /**
-         * We could hardcore the matches here, instead of calculating it, but this way,
-         * if there's a compatibility break in `preg_match_all()` between versions,
-         * we'll know about it.
-         * Secondly, now nobody can mess the hardcoded values up.
-         */
-        $pattern = '/(?<firstName>(?<initial>[A-Z])[a-z]+)(?: (?<surname>[A-Z][a-z]+))?/';
-        preg::match_all($pattern, self::subject, $matches, \PREG_OFFSET_CAPTURE);
-
         $matches = new RawMatchesOffset($matches);
         return new ReplaceDetailImpl(
             new MatchDetail(
-                new Subject(self::subject),
+                new Subject($subject),
                 0,
                 -1,
                 new RawMatchesToMatchAdapter($matches, 0),
                 new EagerMatchAllFactory($matches),
                 new UserData(),
-                new ReplaceMatchGroupFactoryStrategy(-1, '')
-            ),
+                new ReplaceMatchGroupFactoryStrategy(-1, '')),
             $offsetModification,
-            '');
+            $subject);
     }
 }
