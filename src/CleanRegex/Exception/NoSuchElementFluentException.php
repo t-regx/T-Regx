fdb77b2857ce82dc0ea6157974e2f5f6cf6ff5ef
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-02-26 22:37:02 +0100

    Remove unnecessary NoSuchElementFluentException.withMessage()

--- src/CleanRegex/Exception/NoSuchElementFluentException.php
+++ src/CleanRegex/Exception/NoSuchElementFluentException.php
@@ -1,17 +1,10 @@
 <?php
 namespace TRegx\CleanRegex\Exception;
 
-use TRegx\CleanRegex\Internal\Exception\Messages\NotMatchedMessage;
-
 class NoSuchElementFluentException extends \Exception implements PatternException
 {
     public function __construct(string $message)
     {
         parent::__construct($message);
-    }
-
-    public static function withMessage(NotMatchedMessage $message): self
-    {
-        return new self($message->getMessage());
     }
 }
