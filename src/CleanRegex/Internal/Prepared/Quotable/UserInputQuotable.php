196d59735955986a0a2c47dcfea6f699b5d7f130
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-03-07 15:58:53 +0100

    Fix building whitespace in extended mode

--- src/CleanRegex/Internal/Prepared/Quotable/UserInputQuotable.php
+++ src/CleanRegex/Internal/Prepared/Quotable/UserInputQuotable.php
@@ -22,11 +22,11 @@ class UserInputQuotable implements Quotable
     {
         return \strtr($text, [
             ' '    => '\ ',
-            "\t"   => "\\\t",   #9
-            "\n"   => "\\\n",   #10
-            "\x0B" => "\\\x0B", #11
-            "\f"   => "\\\f",   #12
-            "\r"   => "\\\r",   #13
+            "\t"   => '\t',   #9
+            "\n"   => '\n',   #10
+            "\x0B" => '\x0B', #11
+            "\f"   => '\f',   #12
+            "\r"   => '\r',   #13
         ]);
     }
 }
--- test/Unit/CleanRegex/Internal/Prepared/Quotable/UserInputQuotableTest.php
+++ test/Unit/CleanRegex/Internal/Prepared/Quotable/UserInputQuotableTest.php
@@ -28,16 +28,15 @@ class UserInputQuotableTest extends TestCase
     {
         return [
             'empty'     => ['', ''],
-            'slash'     => ['/', '\\/'],
+            'slash'     => ['/', '\/'],
             'backslash' => ['\\', '\\\\'],
             'hash'      => ['Foo#Bar', 'Foo\#Bar'],
-
-            'space' => ['Foo Bar', 'Foo\ Bar'],
-            '\n'    => ["Foo\nBar", "Foo\\\nBar"],
-            '\r'    => ["Foo\rBar", "Foo\\\rBar"],
-            '\t'    => ["Foo\tBar", "Foo\\\tBar"],
-            '\f'    => ["Foo\fBar", "Foo\\\fBar"],
-            '\x0B'  => ["Foo\x0BBar", "Foo\\\x0BBar"],
+            'space'     => ['Foo Bar', 'Foo\ Bar'],
+            '\n'        => ["Foo\nBar", 'Foo\nBar'],
+            '\r'        => ["Foo\rBar", 'Foo\rBar'],
+            '\t'        => ["Foo\tBar", 'Foo\tBar'],
+            '\f'        => ["Foo\fBar", 'Foo\fBar'],
+            '\x0B'      => ["Foo\x0BBar", 'Foo\x0BBar'],
         ];
     }
 }
