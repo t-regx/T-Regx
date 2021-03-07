0e730f185bb46a8d9551dce61a3ef85080191215
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-03-07 17:33:38 +0100

    Add missing /builder/_extended/PatternBuilderTest

---
+++ test/Feature/CleanRegex/PatternBuilder/builder/_extended/PatternBuilderTest.php
@@ -0,0 +1,38 @@
+<?php
+namespace Test\Feature\TRegx\CleanRegex\PatternBuilder\builder\_extended;
+
+use PHPUnit\Framework\TestCase;
+use TRegx\CleanRegex\Pattern;
+
+class PatternBuilderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldBuild_newsLines()
+    {
+        // given
+        $pattern = Pattern::inject("#@\npattern", ["user\ninput"]);
+
+        // when
+        $match = $pattern->match("#user\ninput\npattern")->first();
+
+        // then
+        $this->assertSame("#user\ninput\npattern", $match);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBuild_newsLines_ExtendedMode()
+    {
+        // given
+        $pattern = Pattern::inject("#@\npattern", ["user\ninput"], 'x');
+
+        // when
+        $match = $pattern->match("#user\ninput\npattern")->first();
+
+        // then
+        $this->assertSame('pattern', $match);
+    }
+}
