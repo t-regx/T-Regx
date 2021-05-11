6396320d721853e1a78acf379a4e6c6b4d7f8538
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-05-11 22:24:17 +0200

    Update PcreTest with more PCRE versions

--- test/Functional/PcreTest.php
+++ test/Functional/PcreTest.php
@@ -28,30 +28,51 @@ class PcreTest extends TestCase
 
     public function pcreVersionDependant(): array
     {
+        if (PHP_VERSION_ID >= 80005) {
+            return $this->version(10, 34);
+        }
+        if (PHP_VERSION_ID >= 80003) {
+            return $this->version(10, 35);
+        }
+        if (PHP_VERSION_ID >= 80000) {
+            return $this->version(10, 34);
+        }
+        if (PHP_VERSION_ID >= 70418) {
+            return $this->version(10, 34);
+        }
         if (PHP_VERSION_ID >= 70412) {
-            return $this->version(10, 35, true);
+            return $this->version(10, 35);
         }
         if (PHP_VERSION_ID >= 70406) {
-            return $this->version(10, 34, true);
+            return $this->version(10, 34);
         }
         if (PHP_VERSION_ID >= 70400) {
-            return $this->version(10, 33, true);
+            return $this->version(10, 33);
+        }
+        if (PHP_VERSION_ID >= 70328) {
+            return $this->version(10, 36);
         }
         if (PHP_VERSION_ID >= 70300) {
-            return $this->version(10, 32, true);
+            return $this->version(10, 32);
+        }
+        if (PHP_VERSION_ID >= 70234) {
+            return $this->version(8, 44);
         }
         if (PHP_VERSION_ID >= 70200) {
-            return $this->version(8, 41, false);
+            return $this->version(8, 41);
+        }
+        if (PHP_VERSION_ID >= 70133) {
+            return $this->version(8, 44);
         }
         if (PHP_VERSION_ID >= 70003) {
-            return $this->version(8, 38, false);
+            return $this->version(8, 38);
         }
-        return $this->version(8, 37, false);
+        return $this->version(8, 37);
     }
 
-    private function version(int $major, int $minor, bool $pcre2): array
+    private function version(int $major, int $minor): array
     {
-        return [PHP_VERSION => ["$major.$minor", $major, $minor, $pcre2]];
+        return [PHP_VERSION => ["$major.$minor", $major, $minor, $major >= 10]];
     }
 
     /**
