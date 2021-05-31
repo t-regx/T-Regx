d83f2f1f17b34733e22d9da83b7116f94ee2b55f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-05-31 21:38:57 +0200

    Arrays::flatten() works for empty arrays

--- src/CleanRegex/Internal/Arrays.php
+++ src/CleanRegex/Internal/Arrays.php
@@ -5,6 +5,9 @@ class Arrays
 {
     public static function flatten(array $array): array
     {
+        if (empty($array)) {
+            return [];
+        }
         return \array_merge(...$array);
     }
 
--- test/Unit/CleanRegex/Internal/ArraysTest.php
+++ test/Unit/CleanRegex/Internal/ArraysTest.php
@@ -9,6 +9,16 @@ class ArraysTest extends TestCase
     /**
      * @test
      */
+    public function shouldFlatten()
+    {
+        $this->assertSame([], Arrays::flatten([]));
+        $this->assertSame([], Arrays::flatten([[]]));
+        $this->assertSame(['a', 'b', 'c'], Arrays::flatten([['a', 'b'], ['c']]));
+    }
+
+    /**
+     * @test
+     */
     public function shouldGetDuplicates()
     {
         $this->assertSame(['a', 'b', 'c'], Arrays::getDuplicates(['1', 'a', 'b', 'd', 'c', 'b', 'a', 'e', 'c', 'b', 'c', 'b', 'a']));
