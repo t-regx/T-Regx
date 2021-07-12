41e7dbdcdf0e8648eef8c9f8cbe02c0df7aeb828
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-07-12 03:06:50 +0200

    Remove the now unused remnants of the old prepared patterns implementation

--- src/CleanRegex/Internal/InternalPattern.php
+++ src/CleanRegex/Internal/InternalPattern.php
@@ -1,8 +1,6 @@
 <?php
 namespace TRegx\CleanRegex\Internal;
 
-use TRegx\CleanRegex\Internal\Delimiter\AutomaticDelimiter;
-
 class InternalPattern
 {
     /** @var string */
@@ -14,16 +12,5 @@ class InternalPattern
     {
         $this->pattern = $pattern;
         $this->undevelopedInput = $undevelopedInput;
-    }
-
-    public static function standard(string $pattern, string $flags): InternalPattern
-    {
-        TrailingBackslash::throwIfHas($pattern);
-        return new self(AutomaticDelimiter::standard($pattern, $flags), $pattern);
-    }
-
-    public static function pcre(string $pattern): InternalPattern
-    {
-        return new self($pattern, $pattern);
     }
 }
--- src/CleanRegex/Internal/TrailingBackslash.php
+++ src/CleanRegex/Internal/TrailingBackslash.php
@@ -1,17 +1,8 @@
 <?php
 namespace TRegx\CleanRegex\Internal;
 
-use TRegx\CleanRegex\Internal\Delimiter\TrailingBackslashException;
-
 class TrailingBackslash
 {
-    public static function throwIfHas(string $pattern): void
-    {
-        if (self::hasTrailingSlash($pattern)) {
-            throw new TrailingBackslashException();
-        }
-    }
-
     public static function hasTrailingSlash(string $pattern): bool
     {
         return \substr(\str_replace('\\\\', '', $pattern), -1) === '\\';
