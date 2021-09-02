7efb738c7e090062503335e9d4daa913fc35ef06
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-09-03 01:26:29 +0200

    Update comment in EntryPoints

--- src/CleanRegex/Internal/EntryPoints.php
+++ src/CleanRegex/Internal/EntryPoints.php
@@ -54,26 +54,32 @@ trait EntryPoints
     {
         return new CompositePattern(InternalPatterns::compose($patterns, static function (Pattern $pattern): Definition {
             /**
-             * Pattern instance has reference to InternalPattern as "pattern" private field.
-             * InternalPattern has "pattern" field, containing a delimited PCRE pattern with
-             * flags as a string, and another field, "originalPattern", kept for debugging
-             * purposes in client applications. It contains whatever string input was used
-             * to construct the real pattern. It can either be the exact pcre string,
-             * it can be undelimited pattern used with standard strategy, template with
-             * placeholders in case of prepared pattern, or it can be user input in case
-             * of a mask with keywords.
+             * {@see Pattern} instance has reference to {@see Definition} as "pattern"
+             * private field. Definition contains {@see Definition::$pattern} field,
+             * containing a delimited PCRE pattern withs flags as a string, and another
+             * field {@see Definition::$undevelopedInput}, containing pattern before it
+             * has been parsed - kept for debugging purposes in client applications.
+             * It contains whatever string input was used to construct the real pattern.
+             * It can either be the exact pcre string, it can be undelimited pattern
+             * used with standard strategy, template with placeholders in case of
+             * prepared pattern, or it can be user input in case of a mask with keywords.
+             *
+             * In the future, we plan to make {@see Definition::$undevelopedInput} visible
+             * in the public API for clients, perhas as a field in thrown exceptions, that
+             * clients could use for debugging.
              *
              * In order to use composite patterns, we consume strings and Patterns as an
-             * input, and construct InternalPattern instances with that. In case of a string,
-             * that's simple, we construct a new InternalPattern with that. In case of a Pattern
-             * instance, we could use the delimited pattern and call it a day, but then the
-             * originalPattern of the internal InternalPattern would be lost for the debugging
-             * purposes in the client applications. So to preserve that, we don't construct
-             * new InternalPattern instances, but we take one from the Pattern. Normally, that
-             * would be impossible without reflection, breaking encapsulation or exposing the
-             * internal pattern outside of Pattern (which would break its point of being internal),
-             * but EntryPoints is a trait in Pattern, so it has access to its private fields.
-             * That's why we can just pass a closure, which can map Pattern to InternalPattern.
+             * input, and construct {@see Definition} instances with that. In case of a string,
+             * that's simple, we construct a new {@see Definition} with that. In case of a
+             * {@see Pattern} instance, we could use the delimited pattern and call it a day,
+             * but then the {@see Definition::$undevelopedInput} of the definition  would be
+             * lost for the debugging purposes in the client applications. So to preserve that,
+             * we don't construct new {@see Definition} instances, but we take one from the
+             * {@see Pattern}. Normally, that would be impossible without reflection, breaking
+             * encapsulation or exposing the internal pattern outside of {@see Pattern} (which
+             * would break its point of being internal), but {@see EntryPoints} is a trait in
+             * {@see Pattern}, so it has access to its private fields. That's why we can just
+             * pass a closure, which can map {@see Pattern} to {@see Definition}.
              */
             return $pattern->definition;
         }));
