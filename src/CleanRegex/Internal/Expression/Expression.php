d6186ae7a530154869c6d6c8f5c463dcfc7d912f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-09-06 06:59:46 +0200

    Add dictionary definition to Expression

--- src/CleanRegex/Internal/Expression/Expression.php
+++ src/CleanRegex/Internal/Expression/Expression.php
@@ -3,6 +3,10 @@ namespace TRegx\CleanRegex\Internal\Expression;
 
 use TRegx\CleanRegex\Internal\Definition;
 
+/**
+ * Dictionary definition
+ * <i>Expression</i> - a word or phrase, especially an idiomatic one, used to convey an idea.
+ */
 interface Expression
 {
     public function definition(): Definition;
