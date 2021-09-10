12275201e383717ef2f53cd664f7aaadca622e45
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-09-10 13:34:26 +0200

    Remove unused QuotableFactory

--- src/CleanRegex/Internal/Prepared/Quotable/Factory/QuotableFactory.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace TRegx\CleanRegex\Internal\Prepared\Quotable\Factory;
-
-use TRegx\CleanRegex\Internal\Prepared\Quotable\Quotable;
-
-interface QuotableFactory
-{
-    public function quotable($value): Quotable;
-}
