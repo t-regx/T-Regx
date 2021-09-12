27d0f308afefc1fc39c7fdf23599f5d1f4837360
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-09-12 18:51:00 +0200

    Move tests from DelimiterTest to CandidatesTest

---
+++ test/Unit/CleanRegex/Internal/CandidatesTest.php
@@ -0,0 +1,65 @@
+<?php
+namespace Test\Unit\TRegx\CleanRegex\Internal;
+
+use PHPUnit\Framework\TestCase;
+use TRegx\CleanRegex\Internal\Candidates;
+use TRegx\CleanRegex\Internal\Delimiter\Delimiter;
+use TRegx\CleanRegex\Internal\Delimiter\UndelimiterablePatternException;
+use TRegx\CleanRegex\Internal\UnsuitableStringCondition;
+
+/**
+ * @covers \TRegx\CleanRegex\Internal\Candidates
+ */
+class CandidatesTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider delimiterables
+     */
+    public function shouldBeDelimiterable(string $delimiterable, string $expectedDelimiter)
+    {
+        // given
+        $candidates = new Candidates(new UnsuitableStringCondition($delimiterable));
+
+        // when
+        $pattern = $candidates->delimiter();
+
+        // then
+        $this->assertEquals(new Delimiter($expectedDelimiter), $pattern);
+    }
+
+    public function delimiterables(): array
+    {
+        return [
+            ['foo', '/'],
+            ['foo/bar', '#'],
+            ['foo/bar#cat', '%'],
+            ['foo/bar#cat%', '~'],
+            ['s~i/e%#', '+'],
+            ['s~i/e#++m%a', '!'],
+            ['s~i/e#++m%a!', '@'],
+            ['s~i/e#++m%a!@', '_'],
+            ['s~i/e#+%!@_', ';'],
+            ['s~i/e#+%!@;_', '`'],
+            ['s~i/e#+`%!@;_', '-'],
+            ['s~i/e-#+`%!@;_', '='],
+            ['s~i/e-#+`%=!@;_', ','],
+            ['s~i/,e-#+`%=!@;_', "\1"],
+        ];
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowOnNotEnoughDelimiters()
+    {
+        // given
+        $candidates = new Candidates(new UnsuitableStringCondition("s~i/e#++m%a!@*`_-;=,\1"));
+
+        // then
+        $this->expectException(UndelimiterablePatternException::class);
+
+        // when
+        $candidates->delimiter();
+    }
+}
--- test/Unit/CleanRegex/Internal/Delimiter/DelimiterTest.php
+++ test/Unit/CleanRegex/Internal/Delimiter/DelimiterTest.php
@@ -2,9 +2,7 @@
 namespace Test\Unit\TRegx\CleanRegex\Internal\Delimiter;
 
 use PHPUnit\Framework\TestCase;
-use Test\Utils\Impl\VerifyWord;
 use TRegx\CleanRegex\Internal\Delimiter\Delimiter;
-use TRegx\CleanRegex\Internal\Delimiter\UndelimiterablePatternException;
 use TRegx\CleanRegex\Internal\Flags;
 use TRegx\CleanRegex\Internal\Prepared\Word\TextWord;
 
@@ -37,57 +35,9 @@ class DelimiterTest extends TestCase
         $delimiter = new Delimiter('#');
 
         // when
-        $pattern = $delimiter->delimited(new TextWord('foo/bar#cat'), new Flags('i'));
+        $delimited = $delimiter->delimited(new TextWord('foo/bar#cat'), new Flags('i'));
 
         // then
-        $this->assertSame('#foo/bar\#cat#i', $pattern);
-    }
-
-    /**
-     * @test
-     * @dataProvider delimiterables
-     */
-    public function shouldBeDelimiterable(string $delimiterable, string $expectedDelimiter)
-    {
-        // given
-        $delimiter = Delimiter::suitable($delimiterable);
-
-        // when
-        $pattern = $delimiter->delimited(new VerifyWord('X', $expectedDelimiter), new Flags(''));
-
-        // then
-        $this->assertSame("{$expectedDelimiter}X{$expectedDelimiter}", $pattern);
-    }
-
-    public function delimiterables(): array
-    {
-        return [
-            ['foo', '/'],
-            ['foo/bar', '#'],
-            ['foo/bar#cat', '%'],
-            ['foo/bar#cat%', '~'],
-            ['s~i/e%#', '+'],
-            ['s~i/e#++m%a', '!'],
-            ['s~i/e#++m%a!', '@'],
-            ['s~i/e#++m%a!@', '_'],
-            ['s~i/e#+%!@_', ';'],
-            ['s~i/e#+%!@;_', '`'],
-            ['s~i/e#+`%!@;_', '-'],
-            ['s~i/e-#+`%!@;_', '='],
-            ['s~i/e-#+`%=!@;_', ','],
-            ['s~i/,e-#+`%=!@;_', "\1"],
-        ];
-    }
-
-    /**
-     * @test
-     */
-    public function shouldThrowOnNotEnoughDelimiters()
-    {
-        // then
-        $this->expectException(UndelimiterablePatternException::class);
-
-        // when
-        Delimiter::suitable("s~i/e#++m%a!@*`_-;=,\1");
+        $this->assertSame('#foo/bar\#cat#i', $delimited);
     }
 }
