2c02e02d0eeb36a56324d40ba5de1a7c15b46cbc
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-09-12 13:47:10 +0200

    Add Prepared\Template\MaskToken test

---
+++ test/Unit/CleanRegex/Internal/Prepared/Template/MaskTokenTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace Test\Unit\TRegx\CleanRegex\Internal\Prepared\Template;
+
+use PHPUnit\Framework\TestCase;
+use TRegx\CleanRegex\Exception\MaskMalformedPatternException;
+use TRegx\CleanRegex\Internal\Prepared\Template\MaskToken;
+use TRegx\SafeRegex\Internal\Errors\Errors\EmptyHostError;
+use TRegx\SafeRegex\Internal\Errors\ErrorsCleaner;
+
+/**
+ * @covers \TRegx\CleanRegex\Internal\Prepared\Template\MaskToken
+ */
+class MaskTokenTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider \Test\DataProviders::invalidStandardPatterns()
+     * @param string $invalidPattern
+     */
+    public function shouldNotValidateStandardPattern(string $invalidPattern)
+    {
+        // given
+        $mask = new MaskToken('%s', ['%f' => $invalidPattern]);
+
+        // then
+        $this->expectException(MaskMalformedPatternException::class);
+        $this->expectExceptionMessage("Malformed pattern '$invalidPattern' assigned to keyword '%f'");
+
+        // when
+        $mask->word();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldStandardNotLeaveErrors()
+    {
+        // given
+        $mask = new MaskToken('%s', ['%s' => '/{2,1}/']);
+
+        // when
+        try {
+            $mask->word();
+        } catch (MaskMalformedPatternException $silenced) {
+        }
+
+        // then
+        $this->assertErrorsEmpty();
+    }
+
+    private function assertErrorsEmpty(): void
+    {
+        $errorsCleaner = new ErrorsCleaner();
+        $this->assertInstanceOf(EmptyHostError::class, $errorsCleaner->getError());
+    }
+}
