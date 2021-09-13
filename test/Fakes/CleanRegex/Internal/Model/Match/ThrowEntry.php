1933092aa27330ccd1bbd23704cb490afb00ef3b
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-09-13 20:32:56 +0200

    Clean ThrowEntry

--- test/Fakes/CleanRegex/Internal/Model/Match/ThrowEntry.php
+++ test/Fakes/CleanRegex/Internal/Model/Match/ThrowEntry.php
@@ -8,11 +8,11 @@ class ThrowEntry implements Entry
 {
     public function text(): string
     {
-        throw new AssertionError("Failed to assert that MatchEntry wasn't used");
+        throw new AssertionError("Failed to assert that Entry wasn't used");
     }
 
     public function byteOffset(): int
     {
-        throw new AssertionError("Failed to assert that MatchEntry wasn't used");
+        throw new AssertionError("Failed to assert that Entry wasn't used");
     }
 }
