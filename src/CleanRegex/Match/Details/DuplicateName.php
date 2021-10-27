464488cb31cbfa438362c0093ce55be28ba56c6f
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2021-10-27 22:34:01 +0200

    Update DuplicateName comment

--- src/CleanRegex/Match/Details/DuplicateName.php
+++ src/CleanRegex/Match/Details/DuplicateName.php
@@ -17,34 +17,35 @@ use TRegx\CleanRegex\Match\Details\Group\DuplicateNamedGroup;
 
 /**
  * There are two strategies to handling duplicate names in
- * groups (and to handling any group, actually, though in
+ * groups (and to handling any group, actually, although in
  * non-duplicate names their results are identical).
  *
- * When there are duplicate group names, used in a pattern,
+ * When there are duplicate group names used in a pattern,
  * only the last encountered group will be present in the
  * resulting array, because PHP only keeps unique keys
- * in an array. So even if two groups (duplicate) matched
- * two separate strings, only one will be present. Thus,
- * it's impossible to distinguish which exact (indexed)
- * group actually matched the name group. It's possible
- * to sometimes "guess" that certain groups definitely
- * didn't match it (if their values and/or offsets are
- * different), but if they're equal (and it's possible that
- * they're equal), then the distinguishment is impossible.
+ * in an array. So even if multiple groups (with duplicate
+ * names) matched separate strings, only the last will be
+ * present in the result. Thus, it's impossible to distinguish
+ * which exact (indexed) group actually matched the named
+ * group. It's possible to sometimes "guess" that certain
+ * groups definitely didn't match it (if their values and/or
+ * offsets are different), but if they're equal (and it's
+ * possible that they're equal), then the distinguishment
+ * is impossible.
  *
- * So there are two strategies, to read a group value.
+ * So there are two strategies to read a group value.
  * They have their advantages and flaws.
  *
- * - Read a group as it was at a compiled pattern
+ * - Read a group as it was at compiled pattern
  * - Read a group directly from the runtime result
  *
- * If you read a group at a compile time, it becomes
+ * If you read a group at compile time, it becomes
  * possible to uniquely assign an index to a group,
  * by assuming that the first group in the pattern
  * because "the group", and other groups named with
  * the same names are treated as though they aren't
  * named at all. Basically, removing duplicate names
- * from the pattern, but the first, if there are any.
+ * from the pattern but the first, if there are any.
  *
  * If you read a group at runtime, it becomes slightly
  * more useful, since "in some cases" you can read the
@@ -59,14 +60,14 @@ use TRegx\CleanRegex\Match\Details\Group\DuplicateNamedGroup;
  * Compile strategy:
  * Pattern "(?<g>one)?(?<g>two)?(?<g>three)?" is treated
  * as "(?<g>one)(two)(three)". We just assume the
- * first indexed group is named, other two aren't.
+ * first indexed group is named, and the other two aren't.
  *
  * Runtime strategy:
  * With pattern "((?<g>one)|(?<g>two))" depending on the
  * subject, only the first or the second group will be matched,
  * we'll know that group "g" was matched either "one" or "two",
  * but we can't tell what index did the group have. We can't
- * distinguish them.
+ * distinguish them, because PHP sucks big time.
  *
  * The real explanation:
  *
