39f80cf467d70ea40f021d0616250fb974e460da
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-03-19 11:55:20 +0100

    Remove Number, an unnecessary abstraction

--- src/CleanRegex/Internal/Match/Stream/IntegerStream.php
+++ src/CleanRegex/Internal/Match/Stream/IntegerStream.php
@@ -2,10 +2,8 @@
 namespace TRegx\CleanRegex\Internal\Match\Stream;
 
 use TRegx\CleanRegex\Exception\InvalidIntegerTypeException;
-use TRegx\CleanRegex\Internal\Match\Stream\Number\IdentityNumber;
-use TRegx\CleanRegex\Internal\Match\Stream\Number\IntableNumber;
-use TRegx\CleanRegex\Internal\Match\Stream\Number\Number;
-use TRegx\CleanRegex\Internal\Match\Stream\Number\StreamNumber;
+use TRegx\CleanRegex\Internal\Match\Numeral\MatchBase;
+use TRegx\CleanRegex\Internal\Match\Numeral\StreamExceptions;
 use TRegx\CleanRegex\Internal\Numeral\Base;
 use TRegx\CleanRegex\Internal\Type\ValueType;
 use TRegx\CleanRegex\Match\Details\Intable;
@@ -18,38 +16,36 @@ class IntegerStream implements Upstream
     private $upstream;
     /** @var Base */
     private $base;
+    /** @var MatchBase */
+    private $matchBase;
 
     public function __construct(Upstream $upstream, Base $base)
     {
         $this->upstream = $upstream;
         $this->base = $base;
+        $this->matchBase = new MatchBase($this->base, new StreamExceptions());
     }
 
     public function all(): array
     {
-        return \array_map([$this, 'parsedNumber'], $this->upstream->all());
+        return \array_map([$this, 'number'], $this->upstream->all());
     }
 
     public function first(): int
     {
-        return $this->parsedNumber($this->upstream->first());
+        return $this->number($this->upstream->first());
     }
 
-    private function parsedNumber($value): int
-    {
-        return $this->number($value)->toInt();
-    }
-
-    private function number($value): Number
+    private function number($value): int
     {
         if (\is_int($value)) {
-            return new IdentityNumber($value);
+            return $value;
         }
         if ($value instanceof Intable) {
-            return new IntableNumber($value, $this->base);
+            return $value->toInt($this->base->base());
         }
         if (\is_string($value)) {
-            return new StreamNumber($value, $this->base);
+            return $this->matchBase->integer($value);
         }
         throw InvalidIntegerTypeException::forInvalidType(new ValueType($value));
     }
