2ba6839ee9c6e6d7b7141fc238237650fbc8155c
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-03-24 04:05:17 +0100

    Refactor MatchIntStreamTest from Unit/ test to Feature/ test

--- test/Feature/CleanRegex/Match/asInt/AbstractMatchPatternTest.php
+++ test/Feature/CleanRegex/Match/asInt/AbstractMatchPatternTest.php
@@ -7,11 +7,17 @@ use Test\Fakes\CleanRegex\Internal\ThrowSubject;
 use Test\Utils\CustomSubjectException;
 use Test\Utils\ExactExceptionMessage;
 use Test\Utils\Functions;
+use TRegx\CleanRegex\Exception\IntegerFormatException;
+use TRegx\CleanRegex\Exception\IntegerOverflowException;
 use TRegx\CleanRegex\Exception\NoSuchNthElementException;
 use TRegx\CleanRegex\Exception\NoSuchStreamElementException;
 use TRegx\CleanRegex\Exception\SubjectNotMatchedException;
 use TRegx\CleanRegex\Internal\Match\Stream\StreamRejectedException;
+use TRegx\CleanRegex\Pattern;
 
+/**
+ * @covers \TRegx\CleanRegex\Internal\Match\Stream\Base\MatchIntStream
+ */
 class AbstractMatchPatternTest extends TestCase
 {
     use ExactExceptionMessage;
@@ -262,5 +268,144 @@ class AbstractMatchPatternTest extends TestCase
 
         // when
         pattern('\d+')->match('Foo')->asInt()->stream()->first();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetAllAsInt()
+    {
+        // given
+        $stream = Pattern::of('\d+')->match('14, 19, 25')->asInt();
+
+        // when
+        $all = $stream->all();
+
+        // then
+        $this->assertSame([14, 19, 25], $all);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetEmptyForUnmatchedSubject()
+    {
+        // given
+        $stream = Pattern::of('Faramir')->match('I love you, Boromir')->asInt();
+
+        // when
+        $all = $stream->all();
+
+        // then
+        $this->assertSame([], $all);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetFirstInBase2()
+    {
+        // given
+        $stream = Pattern::of('\d+')->match('10101')->asInt(2);
+
+        // when
+        $first = $stream->first();
+
+        // then
+        $this->assertSame(21, $first);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetFirstLowerBound()
+    {
+        // given
+        $stream = Pattern::of('-\d+')->match('-2147483648')->asInt();
+
+        // when
+        $first = $stream->first();
+
+        // then
+        $this->assertSame(-2147483647 - 1, $first); // for some reason, PHP parses -2147483648 as float ;|
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetFirstKey()
+    {
+        // given
+        $stream = Pattern::of('\d+')->match('192')->asInt();
+
+        // when
+        $key = $stream->keys()->first();
+
+        // then
+        $this->assertSame(0, $key);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrow_all_forMalformedInteger()
+    {
+        // given
+        $stream = Pattern::of('\w+')->match('Foo, Bar')->asInt();
+
+        // then
+        $this->expectException(IntegerFormatException::class);
+        $this->expectExceptionMessage("Expected to parse 'Foo', but it is not a valid integer in base 10");
+
+        // when
+        $stream->all();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrow_first_forMalformedInteger()
+    {
+        // given
+        $stream = Pattern::of('\w+')->match('Foo, Bar')->asInt(11);
+
+        // then
+        $this->expectException(IntegerFormatException::class);
+        $this->expectExceptionMessage("Expected to parse 'Foo', but it is not a valid integer in base 11");
+
+        // when
+        $stream->first();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrow_firstKey_forMalformedInteger()
+    {
+        // given
+        $stream = Pattern::of('Foo')->match('Foo')->asInt(13);
+
+        // then
+        $this->expectException(IntegerFormatException::class);
+        $this->expectExceptionMessage("Expected to parse 'Foo', but it is not a valid integer in base 13");
+
+        // when
+        $stream->keys()->first();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrow_firstKey_forOverflownInteger()
+    {
+        // given
+        $stream = Pattern::of('\d+')->match('922337203685477580700')->asInt(13);
+
+        // then
+        $this->expectException(IntegerOverflowException::class);
+        $this->expectExceptionMessage("Expected to parse '922337203685477580700', but it exceeds integer size on this architecture in base 13");
+
+        // when
+        $stream->keys()->first();
     }
 }
--- test/Feature/CleanRegex/Match/stream/group/MatchGroupStreamTest.php
+++ test/Feature/CleanRegex/Match/stream/group/MatchGroupStreamTest.php
@@ -1,5 +1,5 @@
 <?php
-namespace Test\Feature\TRegx\CleanRegex\Match\stream\group;
+namespace Test\Feature\CleanRegex\Match\stream\group;
 
 use PHPUnit\Framework\TestCase;
 use function pattern;
