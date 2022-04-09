f6400ad640dd3f7f18262eafe5de27c350ebc130
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-04-09 17:20:03 +0200

    Refactor ReplacePatternCallbackInvokerTest from Interaction/ test to Feature/

---
+++ test/Feature/CleanRegex/Replace/callback/ReplacePatternTest.php
@@ -0,0 +1,56 @@
+<?php
+namespace Test\Feature\TRegx\CleanRegex\Replace\callback;
+
+use PHPUnit\Framework\TestCase;
+use Test\Utils\AssertsSameMatches;
+use Test\Utils\Functions;
+use Test\Utils\TestCasePasses;
+use TRegx\CleanRegex\Pattern;
+
+/**
+ * @coversNothing
+ */
+class ReplacePatternTest extends TestCase
+{
+    use AssertsSameMatches, TestCasePasses;
+
+    /**
+     * @test
+     */
+    public function shouldPassOffsets()
+    {
+        // given
+        $subject = 'Tom Cruise is 21 years old and has 192cm';
+        // when
+        Pattern::of('[0-9]+')
+            ->replace($subject)
+            ->first()
+            ->callback(Functions::out($detail, ''));
+        // then
+        $this->assertSame(14, $detail->offset());
+        $this->assertSame($subject, $detail->subject());
+        $this->assertSame(['21', '192'], $detail->all());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldInvokeUpToLimit()
+    {
+        // when
+        Pattern::of('.')->replace('Lorem')->only(3)->callback(Functions::collect($details, ''));
+        // then
+        $this->assertSame(3, \count($details));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldNotInvokeCallback()
+    {
+        // given
+        Pattern::of('Foo')->replace('Bar')->callback(Functions::fail());
+        // then
+        $this->pass();
+    }
+}
