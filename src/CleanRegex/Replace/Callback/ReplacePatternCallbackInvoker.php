6d8c32772638a5b6c1929774eac4732f0de23417
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-04-16 12:53:32 +0200

    Extract GroupSubstitute from ReplacePatternCallbackInvoker

--- src/CleanRegex/Replace/Callback/ReplacePatternCallbackInvoker.php
+++ src/CleanRegex/Replace/Callback/ReplacePatternCallbackInvoker.php
@@ -1,13 +1,13 @@
 <?php
 namespace TRegx\CleanRegex\Replace\Callback;
 
-use TRegx\CleanRegex\Exception\NonexistentGroupException;
 use TRegx\CleanRegex\Internal\Definition;
 use TRegx\CleanRegex\Internal\GroupKey\GroupKey;
 use TRegx\CleanRegex\Internal\Model\GroupAware;
 use TRegx\CleanRegex\Internal\Model\LightweightGroupAware;
 use TRegx\CleanRegex\Internal\Pcre\Legacy\ApiBase;
 use TRegx\CleanRegex\Internal\Pcre\Legacy\LazyMatchAllFactory;
+use TRegx\CleanRegex\Internal\Pcre\Legacy\MatchAllFactory;
 use TRegx\CleanRegex\Internal\Replace\By\NonReplaced\SubjectRs;
 use TRegx\CleanRegex\Internal\Replace\Counting\CountingStrategy;
 use TRegx\CleanRegex\Internal\Subject;
@@ -21,15 +21,16 @@ class ReplacePatternCallbackInvoker
     private $subject;
     /** @var int */
     private $limit;
-    /** @var SubjectRs */
-    private $substitute;
     /** @var CountingStrategy */
     private $countingStrategy;
     /** @var GroupAware */
     private $groupAware;
     /** @var GroupKey */
     private $group;
+    /** @var MatchAllFactory */
     private $allFactory;
+    /** @var GroupSubstitute */
+    private $substitute;
 
     public function __construct(Definition       $definition,
                                 Subject          $subject,
@@ -42,11 +43,11 @@ class ReplacePatternCallbackInvoker
         $this->definition = $definition;
         $this->subject = $subject;
         $this->limit = $limit;
-        $this->substitute = $substitute;
         $this->countingStrategy = $countingStrategy;
         $this->allFactory = new LazyMatchAllFactory(new ApiBase($definition, $subject));
         $this->groupAware = $groupAware;
         $this->group = $group;
+        $this->substitute = new GroupSubstitute($subject, $substitute, $group, $groupAware);
     }
 
     public function invoke(callable $callback, ReplaceCallbackArgumentStrategy $strategy): string
@@ -54,20 +55,9 @@ class ReplacePatternCallbackInvoker
         $result = $this->pregReplaceCallback($callback, $replaced, $strategy);
         $this->countingStrategy->count($replaced, new LightweightGroupAware($this->definition));
         if ($replaced === 0) {
-            if ($this->groupExists()) {
-                return $this->substitute->substitute($this->subject) ?? $result;
-            }
-            throw new NonexistentGroupException($this->group);
+            return $this->substitute->substitute($result);
         }
         return $result;
-    }
-
-    private function groupExists(): bool
-    {
-        if ($this->group->nameOrIndex() === 0) {
-            return true;
-        }
-        return $this->groupAware->hasGroup($this->group);
     }
 
     private function pregReplaceCallback(callable $callback, ?int &$replaced, ReplaceCallbackArgumentStrategy $strategy): string
