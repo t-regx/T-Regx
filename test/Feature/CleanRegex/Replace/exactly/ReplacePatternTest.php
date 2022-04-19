00fc4fad1e1282e37245b09300e5ef14660e4823
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-04-19 23:32:23 +0200

    Refactor ReplacementExpectationFailedExceptionTest from Unit/ test to Feature/

--- test/Feature/CleanRegex/Replace/exactly/ReplacePatternTest.php
+++ test/Feature/CleanRegex/Replace/exactly/ReplacePatternTest.php
@@ -13,7 +13,6 @@ class ReplacePatternTest extends TestCase
     {
         // when
         $result = pattern('Foo')->replace('Foo Bar Bar Bar')->first()->exactly()->with('Bar');
-
         // then
         $this->assertSame('Bar Bar Bar Bar', $result);
     }
@@ -25,7 +24,6 @@ class ReplacePatternTest extends TestCase
     {
         // when
         $result = pattern('Foo')->replace('Foo Foo Bar Bar')->only(2)->exactly()->with('Bar');
-
         // then
         $this->assertSame('Bar Bar Bar Bar', $result);
     }
@@ -38,7 +36,6 @@ class ReplacePatternTest extends TestCase
         // then
         $this->expectException(ReplacementExpectationFailedException::class);
         $this->expectExceptionMessage('Expected to perform exactly 1 replacement(s), but 0 replacement(s) were actually performed');
-
         // when
         pattern('Foo')->replace('Bar Bar Bar Bar')->first()->exactly()->with('Bar');
     }
@@ -51,9 +48,24 @@ class ReplacePatternTest extends TestCase
         // then
         $this->expectException(ReplacementExpectationFailedException::class);
         $this->expectExceptionMessage('Expected to perform exactly 2 replacement(s), but 1 replacement(s) were actually performed');
-
         // when
         pattern('Foo')->replace('Foo Bar Bar Bar')->only(2)->exactly()->with('Bar');
+    }
+
+    /**
+     * @test
+     * @depends shouldThrow_first_exactly_none
+     * @depends shouldThrow_two_exactly_once
+     */
+    public function shouldGetParameters_insufficient(): void
+    {
+        try {
+            pattern('Foo')->replace('Foo Bar Bar Bar')->only(2)->exactly()->with('Bar');
+        } catch (ReplacementExpectationFailedException $exception) {
+            // when + then
+            $this->assertSame(2, $exception->getExpected());
+            $this->assertSame(1, $exception->getReplaced());
+        }
     }
 
     /**
@@ -64,7 +76,6 @@ class ReplacePatternTest extends TestCase
         // then
         $this->expectException(ReplacementExpectationFailedException::class);
         $this->expectExceptionMessage('Expected to perform exactly 1 replacement(s), but at least 2 replacement(s) would have been performed');
-
         // when
         pattern('Foo')->replace('Foo Foo Bar Bar')->first()->exactly()->with('Bar');
     }
@@ -77,8 +88,24 @@ class ReplacePatternTest extends TestCase
         // then
         $this->expectException(ReplacementExpectationFailedException::class);
         $this->expectExceptionMessage('Expected to perform exactly 2 replacement(s), but at least 3 replacement(s) would have been performed');
-
         // when
         pattern('Foo')->replace('Foo Foo Foo Bar')->only(2)->exactly()->with('Bar');
+    }
+
+    /**
+     * @test
+     * @depends shouldThrow_first_exactly_twice
+     * @depends shouldThrow_two_exactly_thrice
+     */
+    public function shouldSuperfluous(): void
+    {
+        // given
+        try {
+            pattern('Foo')->replace('Foo Foo Foo Bar')->only(2)->exactly()->with('Bar');
+        } catch (ReplacementExpectationFailedException $exception) {
+            // when + then
+            $this->assertSame(2, $exception->getExpected());
+            $this->assertSame(3, $exception->getReplaced());
+        }
     }
 }
