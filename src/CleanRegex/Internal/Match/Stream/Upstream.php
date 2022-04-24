20f56ef9e6fb5cb56be124116d68fb62785edb19
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-04-25 01:08:53 +0200

    Remove unnecessary ValueStream

--- src/CleanRegex/Internal/Match/Stream/Upstream.php
+++ src/CleanRegex/Internal/Match/Stream/Upstream.php
@@ -1,8 +1,32 @@
 <?php
 namespace TRegx\CleanRegex\Internal\Match\Stream;
 
-interface Upstream extends ValueStream
+use TRegx\CleanRegex\Internal\Match\Stream\Base\UnmatchedStreamException;
+
+interface Upstream
 {
+    /**
+     * @throws UnmatchedStreamException Supposed to be thrown
+     * when subject backing the stream wasn't matched, to
+     * differentiate an empty stream from a stream that was
+     * empty from the beginning.
+     */
+    public function all(): array;
+
+    /**
+     * @throws UnmatchedStreamException Supposed to be thrown
+     * when subject backing the stream wasn't matched, to
+     * differentiate an empty stream from a stream that was
+     * empty from the beginning.
+     * @throws EmptyStreamException Supposed to be thrown
+     * when the stream is empty and there is no first element,
+     * for example when it was filtered out.
+     * @throws StreamRejectedException Supposed to be thrown
+     * when first element of the stream couldn't be resolved,
+     * because of a match reason (unmatched subject or group).
+     */
+    public function first();
+
     /**
      * @return string|int
      */
