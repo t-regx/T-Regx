395de0ae73cbc70cc560c39748c9114621be4472
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-04-25 04:14:21 +0200

    Refactor KeyStreamTest from Unit/ test to Feature/

---
+++ test/Feature/CleanRegex/Match/stream/keys/MatchPatternTest.php
@@ -0,0 +1,139 @@
+<?php
+namespace Test\Feature\TRegx\CleanRegex\Match\stream\keys;
+
+use PHPUnit\Framework\TestCase;
+use Test\Utils\Functions;
+use Test\Utils\TestCasePasses;
+use TRegx\CleanRegex\Exception\NoSuchStreamElementException;
+use TRegx\CleanRegex\Match\Stream;
+use TRegx\CleanRegex\Pattern;
+use TRegx\Exception\MalformedPatternException;
+
+/**
+ * @covers \TRegx\CleanRegex\Internal\Match\Stream\KeyStream
+ */
+class MatchPatternTest extends TestCase
+{
+    use TestCasePasses;
+
+    /**
+     * @test
+     */
+    public function shouldGetAllKeys()
+    {
+        // when
+        $keys = $this->streamOf(['a' => 'One', 'b' => 'Two', 'c' => 'Three'])->keys()->all();
+        // then
+        $this->assertSame(['a', 'b', 'c'], $keys);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetKeysKeys()
+    {
+        // when
+        $keysKeys = $this->streamOf(['a' => 'One', 'b' => 'Two', 'c' => 'Three'])
+            ->keys()
+            ->keys()
+            ->all();
+        // then
+        $this->assertSame([0, 1, 2], $keysKeys);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetFirstKey()
+    {
+        // when
+        $firstKey = $this->streamOf(['a' => 'One', 'b' => 'Two', 'c' => 'Three'])
+            ->keys()
+            ->first();
+        // then
+        $this->assertSame('a', $firstKey);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldGetFirstKeyKey()
+    {
+        // when
+        $firstKey = $this->streamOf(['a' => 'One', 'b' => 'Two', 'c' => 'Three'])
+            ->keys()
+            ->keys()
+            ->first();
+        // then
+        $this->assertSame(0, $firstKey);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldKeysFirstThrowForUnmatchedStream()
+    {
+        // then
+        $this->expectException(NoSuchStreamElementException::class);
+        $this->expectExceptionMessage('Expected to get the first match, but subject was not matched');
+        // when
+        pattern('Foo')->match('Bar')->stream()->keys()->first();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldKeysKeysFirstThrowForUnmatchedStream()
+    {
+        // then
+        $this->expectException(NoSuchStreamElementException::class);
+        $this->expectExceptionMessage('Expected to get the first match, but subject was not matched');
+        // when
+        pattern('Foo')->match('Bar')->stream()->keys()->keys()->first();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldFirstKeyCallFirstFlatMap()
+    {
+        // when
+        Pattern::of('Foo')->match('Foo')
+            ->stream()
+            ->flatMap(Functions::pass(['key']))
+            ->keys()
+            ->first();
+    }
+
+    /**
+     * @test
+     */
+    public function shouldFirstKeyKeyCallFirstFlatMap()
+    {
+        // when
+        Pattern::of('Foo')->match('Foo')
+            ->stream()
+            ->flatMap(Functions::pass(['key']))
+            ->keys()
+            ->keys()
+            ->first();
+    }
+
+    private function streamOf(array $elements): Stream
+    {
+        return Pattern::of('Foo')->match('Foo')
+            ->stream()
+            ->flatMapAssoc(Functions::constant($elements));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldChainingKeysValidatePattern()
+    {
+        // then
+        $this->expectException(MalformedPatternException::class);
+        // when
+        Pattern::of('+')->match('Foo')->stream()->keys()->keys()->keys()->first();
+    }
+}
