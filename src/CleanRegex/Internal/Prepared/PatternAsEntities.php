51adb2b41f5fca05c3d603242bed6b65edf5a0ba
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-07-10 17:25:21 +0200

    Extract PatternEntities from PatternAsEntities

--- src/CleanRegex/Internal/Prepared/PatternAsEntities.php
+++ src/CleanRegex/Internal/Prepared/PatternAsEntities.php
@@ -3,40 +3,19 @@ namespace TRegx\CleanRegex\Internal\Prepared;
 
 use Generator;
 use TRegx\CleanRegex\Internal\Flags;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\CommentConsumer;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\ControlConsumer;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\EscapeConsumer;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\GroupCloseConsumer;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\GroupConsumer;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\LiteralConsumer;
 use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\PlaceholderConsumer;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\PosixConsumer;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\QuoteConsumer;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Convention;
 use TRegx\CleanRegex\Internal\Prepared\Parser\Entity\Entity;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Feed\Feed;
-use TRegx\CleanRegex\Internal\Prepared\Parser\PcreParser;
 use TRegx\CleanRegex\Internal\Prepared\Phrase\CompositePhrase;
 use TRegx\CleanRegex\Internal\Prepared\Phrase\Phrase;
 
 class PatternAsEntities
 {
-    /** @var PcreParser */
-    private $pcreParser;
+    /** @var PatternEntities */
+    private $entities;
 
     public function __construct(string $pattern, Flags $flags, PlaceholderConsumer $placeholderConsumer)
     {
-        $this->pcreParser = new PcreParser(new Feed($pattern), $flags, [
-            new ControlConsumer(),
-            new QuoteConsumer(),
-            new EscapeConsumer(),
-            new GroupConsumer(),
-            new GroupCloseConsumer(),
-            $placeholderConsumer,
-            new PosixConsumer(),
-            new CommentConsumer(new Convention($pattern)),
-            new LiteralConsumer(),
-        ]);
+        $this->entities = new PatternEntities($pattern, $flags, $placeholderConsumer);
     }
 
     /**
@@ -44,7 +23,7 @@ class PatternAsEntities
      */
     public function entities(): array
     {
-        return $this->pcreParser->entities();
+        return $this->entities->entities();
     }
 
     public function phrase(): Phrase
@@ -54,7 +33,7 @@ class PatternAsEntities
 
     private function phrases(): Generator
     {
-        foreach ($this->pcreParser->entities() as $entity) {
+        foreach ($this->entities->entities() as $entity) {
             yield $entity->phrase();
         }
     }
---
+++ src/CleanRegex/Internal/Prepared/PatternEntities.php
@@ -0,0 +1,46 @@
+<?php
+namespace TRegx\CleanRegex\Internal\Prepared;
+
+use TRegx\CleanRegex\Internal\Flags;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\CommentConsumer;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\ControlConsumer;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\EscapeConsumer;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\GroupCloseConsumer;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\GroupConsumer;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\LiteralConsumer;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\PlaceholderConsumer;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\PosixConsumer;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Consumer\QuoteConsumer;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Convention;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Entity\Entity;
+use TRegx\CleanRegex\Internal\Prepared\Parser\Feed\Feed;
+use TRegx\CleanRegex\Internal\Prepared\Parser\PcreParser;
+
+class PatternEntities
+{
+    /** @var PcreParser */
+    private $pcreParser;
+
+    public function __construct(string $pattern, Flags $flags, PlaceholderConsumer $placeholderConsumer)
+    {
+        $this->pcreParser = new PcreParser(new Feed($pattern), $flags, [
+            new ControlConsumer(),
+            new QuoteConsumer(),
+            new EscapeConsumer(),
+            new GroupConsumer(),
+            new GroupCloseConsumer(),
+            $placeholderConsumer,
+            new PosixConsumer(),
+            new CommentConsumer(new Convention($pattern)),
+            new LiteralConsumer(),
+        ]);
+    }
+
+    /**
+     * @return Entity[]
+     */
+    public function entities(): array
+    {
+        return $this->pcreParser->entities();
+    }
+}
