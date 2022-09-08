d7bdcf938c92fd6d2e621d7e205e0a9fdbbbed8b
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-09-08 04:47:29 +0200

    Update callback/ tests

--- test/Feature/CleanRegex/Replace/callback/Test.php
+++ test/Feature/CleanRegex/Replace/callback/Test.php
@@ -5,8 +5,10 @@ use PHPUnit\Framework\TestCase;
 use Test\Utils\Backtrack\CausesBacktracking;
 use Test\Utils\Functions;
 use Test\Utils\TestCase\TestCasePasses;
+use TRegx\CleanRegex\Exception\InvalidReplacementException;
 use TRegx\CleanRegex\Exception\ReplacementExpectationFailedException;
 use TRegx\CleanRegex\Pattern;
+use TRegx\Exception\MalformedPatternException;
 
 /**
  * @coversNothing
@@ -14,6 +16,55 @@ use TRegx\CleanRegex\Pattern;
 class Test extends TestCase
 {
     use TestCasePasses, CausesBacktracking;
+
+    /**
+     * @test
+     */
+    public function shouldReplace()
+    {
+        // given
+        $pattern = pattern('\w+');
+        // when
+        $replaced = $pattern->replace('Joffrey, Cersei, Ilyn Payne, The Hound')->callback(Functions::charAt(0));
+        // then
+        $this->assertSame('J, C, I P, T H', $replaced);
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrow_forInvalidTypeArray()
+    {
+        // then
+        $this->expectException(InvalidReplacementException::class);
+        $this->expectExceptionMessage('Invalid callback() callback return type. Expected string, but array (0) given');
+        // when
+        pattern('\w+')->replace('Foo')->callback(Functions::constant([]));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrow_forInvalidTypeInteger()
+    {
+        // then
+        $this->expectException(InvalidReplacementException::class);
+        $this->expectExceptionMessage('Invalid callback() callback return type. Expected string, but integer (12) given');
+        // when
+        pattern('Foo')->replace('Foo')->callback(Functions::constant(12));
+    }
+
+    /**
+     * @test
+     */
+    public function shouldThrowMalformedPatternException()
+    {
+        // when
+        $this->expectException(MalformedPatternException::class);
+        $this->expectExceptionMessage('Quantifier does not follow a repeatable item at offset 0');
+        // when
+        pattern('?')->replace('Bar')->callback(Functions::fail());
+    }
 
     /**
      * @test
@@ -40,37 +91,12 @@ class Test extends TestCase
     /**
      * @test
      */
-    public function shouldReturnModifiedValues()
-    {
-        // when
-        $replaced = Pattern::alteration(['sz', 'cz', 'rz', 'ch'], 'i')
-            ->replace('Chrząszcz brzmi w trzcinie, w Szczebrzeszynie')
-            ->callback(Functions::constant('X'));
-        // then
-        $this->assertSame('XXąXX bXmi w tXcinie, w XXebXeXynie', $replaced);
-    }
-
-    /**
-     * @test
-     */
-    public function shouldThrowCatastrophicBacktracking_WhileCheckingLast_callback_first()
+    public function shouldThrowCatastrophicBacktracking_WhileCheckingLast_callback()
     {
         // then
         $this->expectException(ReplacementExpectationFailedException::class);
         $this->expectExceptionMessage('Expected to perform exactly 1 replacement(s), but at least 2 replacement(s) would have been performed');
         // when
         $this->backtrackingReplace(2)->first()->exactly()->callback(Functions::constant('Foo'));
-    }
-
-    /**
-     * @test
-     */
-    public function shouldThrowCatastrophicBacktracking_WhileCheckingLast_callback_only()
-    {
-        // then
-        $this->expectException(ReplacementExpectationFailedException::class);
-        $this->expectExceptionMessage('Expected to perform exactly 1 replacement(s), but at least 2 replacement(s) would have been performed');
-        // when
-        $this->backtrackingReplace(2)->only(1)->exactly()->callback(Functions::constant('Foo'));
     }
 }
