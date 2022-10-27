66099e45890e913139c378d3b71e64d4256f1eb5
Daniel Wilkowski <daniel.wilkowski@fiatrepublic.com>
2022-10-27 23:23:52 +0200

    Refactor ArrayStream to Feature/ test, instead of passing implementation of Stream

--- test/Utils/Stream/ArrayStream.php
+++ test/Utils/Stream/ArrayStream.php
@@ -1,65 +1,32 @@
 <?php
 namespace Test\Utils\Stream;
 
-use TRegx\CleanRegex\Internal\Match\Stream\Base\UnmatchedStreamException;
-use TRegx\CleanRegex\Internal\Match\Stream\EmptyStreamException;
-use TRegx\CleanRegex\Internal\Match\Stream\Upstream;
+use Test\Utils\Functions;
 use TRegx\CleanRegex\Match\Stream;
+use TRegx\CleanRegex\Pattern;
 
 class ArrayStream
 {
     public static function empty(): Stream
     {
-        return new Stream(self::upstream([]));
+        return Pattern::of('Foo')
+            ->match('Foo')
+            ->stream()
+            ->filter(Functions::constant(false));
     }
 
     public static function unmatched(): Stream
     {
-        return new Stream(new class implements Upstream {
-            public function all(): array
-            {
-                throw new UnmatchedStreamException();
-            }
-
-            public function first(): array
-            {
-                throw new UnmatchedStreamException();
-            }
-        });
+        return Pattern::of('(*FAIL)')
+            ->match('subject')
+            ->stream();
     }
 
     public static function of(array $elements): Stream
     {
-        if (empty($elements)) {
-            throw new \AssertionError("Empty stream");
-        }
-        return new Stream(self::upstream($elements));
-    }
-
-    private static function upstream(array $elements): Upstream
-    {
-        return new class($elements) implements Upstream {
-            /** @var array */
-            private $elements;
-
-            public function __construct(array $elements)
-            {
-                $this->elements = $elements;
-            }
-
-            public function all(): array
-            {
-                return $this->elements;
-            }
-
-            public function first(): array
-            {
-                if (empty($this->elements)) {
-                    throw new EmptyStreamException();
-                }
-                $first = \key($this->elements);
-                return [$first, $this->elements[$first]];
-            }
-        };
+        return Pattern::of('Foo')
+            ->search('Foo')
+            ->stream()
+            ->toMap(Functions::constant($elements));
     }
 }
