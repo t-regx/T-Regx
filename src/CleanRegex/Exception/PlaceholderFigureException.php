2d93679a5252848f4858c01dfa8a1b6d605c33f0
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-11-12 15:32:14 +0100

    Remove static factory from PlaceholderFigureException

--- src/CleanRegex/Exception/PlaceholderFigureException.php
+++ src/CleanRegex/Exception/PlaceholderFigureException.php
@@ -3,13 +3,4 @@ namespace TRegx\CleanRegex\Exception;
 
 class PlaceholderFigureException extends \RuntimeException
 {
-    public static function forSuperfluousPlaceholders(int $expected, int $actual): PlaceholderFigureException
-    {
-        return new self("Not enough corresponding figures supplied. Used $expected placeholders, but $actual figures supplied.");
-    }
-
-    public static function forSuperfluousFigures(int $expected, int $actual): self
-    {
-        return new self("Supplied a superfluous figure. Used $expected placeholders, but $actual figures supplied.");
-    }
 }
