d47440428618bb03e7de87954cabe5d9fe0af4ef
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-11-12 16:02:10 +0100

    Inline UnevenCutException constructor

--- src/CleanRegex/Exception/UnevenCutException.php
+++ src/CleanRegex/Exception/UnevenCutException.php
@@ -3,12 +3,4 @@ namespace TRegx\CleanRegex\Exception;
 
 class UnevenCutException extends \RuntimeException implements PatternException
 {
-    public function __construct(bool $patternNotMatched)
-    {
-        if ($patternNotMatched) {
-            parent::__construct("Expected the pattern to make exactly 1 cut, but the pattern doesn't match the subject");
-        } else {
-            parent::__construct("Expected the pattern to make exactly 1 cut, but 2 or more cuts were matched");
-        }
-    }
 }
