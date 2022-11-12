1bdd89ba77e63b9fe113c1b00b7e9712b767f968
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-11-12 15:53:29 +0100

    Remove static factory from IntegerFormatException

--- src/CleanRegex/Exception/IntegerFormatException.php
+++ src/CleanRegex/Exception/IntegerFormatException.php
@@ -1,23 +1,6 @@
 <?php
 namespace TRegx\CleanRegex\Exception;
 
-use TRegx\CleanRegex\Internal\GroupKey\GroupKey;
-use TRegx\CleanRegex\Internal\Numeral\Base;
-
 class IntegerFormatException extends \RuntimeException implements PatternException
 {
-    public static function forMatch(string $value, Base $base): self
-    {
-        return new self("Expected to parse '$value', but it is not a valid integer in base $base");
-    }
-
-    public static function forGroup(GroupKey $group, string $value, Base $base): self
-    {
-        return new self("Expected to parse group $group, but '$value' is not a valid integer in base $base");
-    }
-
-    public static function forStream(string $value, Base $base): self
-    {
-        return new self("Expected to parse stream element '$value', but it is not a valid integer in base $base");
-    }
 }
