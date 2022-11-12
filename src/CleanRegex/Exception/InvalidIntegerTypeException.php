18cc693cd29577d807168e5b64566f44fbfa5d33
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-11-12 15:56:51 +0100

    Remove static factory from InvalidIntegerTypeException

--- src/CleanRegex/Exception/InvalidIntegerTypeException.php
+++ src/CleanRegex/Exception/InvalidIntegerTypeException.php
@@ -1,12 +1,6 @@
 <?php
 namespace TRegx\CleanRegex\Exception;
 
-use TRegx\CleanRegex\Internal\Type\Type;
-
 class InvalidIntegerTypeException extends \RuntimeException implements PatternException
 {
-    public static function forInvalidType(Type $type): self
-    {
-        return new self("Failed to parse value as integer. Expected integer|string, but $type given");
-    }
 }
