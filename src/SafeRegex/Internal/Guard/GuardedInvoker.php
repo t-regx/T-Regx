144e1bb4e342b0dfe84a93d893b3098fa33dd3be
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-12-13 17:11:02 +0100

    Inline ExceptionFactory

--- src/SafeRegex/Internal/Guard/GuardedInvoker.php
+++ src/SafeRegex/Internal/Guard/GuardedInvoker.php
@@ -1,39 +1,52 @@
 <?php
 namespace TRegx\SafeRegex\Internal\Guard;
 
+use TRegx\SafeRegex\Exception\PregException;
+use TRegx\SafeRegex\Exception\SuspectedReturnPregException;
 use TRegx\SafeRegex\Internal\Errors\ErrorsCleaner;
-use TRegx\SafeRegex\Internal\ExceptionFactory;
 use TRegx\SafeRegex\Internal\Guard\Strategy\SuspectedReturnStrategy;
 
 class GuardedInvoker
 {
-    /** @var callable */
-    private $callback;
     /** @var string */
     private $methodName;
-    /** @var ErrorsCleaner */
-    private $errorsCleaner;
-    /** @var ExceptionFactory */
-    private $exceptionFactory;
+    /** @var string|string[] */
+    private $pattern;
+    /** @var callable */
+    private $callback;
     /** @var SuspectedReturnStrategy */
     private $strategy;
+    /** @var ErrorsCleaner */
+    private $errorsCleaner;
 
     public function __construct(string $methodName, $pattern, callable $callback, SuspectedReturnStrategy $strategy)
     {
-        $this->callback = $callback;
         $this->methodName = $methodName;
-        $this->errorsCleaner = new ErrorsCleaner();
+        $this->pattern = $pattern;
+        $this->callback = $callback;
         $this->strategy = $strategy;
-        $this->exceptionFactory = new ExceptionFactory($pattern, $this->strategy, $this->errorsCleaner);
+        $this->errorsCleaner = new ErrorsCleaner();
     }
 
     public function catch(): array
     {
         $this->errorsCleaner->clear();
         $result = ($this->callback)();
-        $exception = $this->exceptionFactory->retrieveGlobals($this->methodName, $result);
+        $exception = $this->retrieveGlobals($this->methodName, $result);
         $this->errorsCleaner->clear();
 
         return [$result, $exception];
+    }
+
+    private function retrieveGlobals(string $methodName, $pregResult): ?PregException
+    {
+        $hostError = $this->errorsCleaner->getError();
+        if ($hostError->occurred()) {
+            return $hostError->getSafeRegexpException($methodName, $this->pattern);
+        }
+        if ($this->strategy->isSuspected($methodName, $pregResult)) {
+            return new SuspectedReturnPregException($methodName, $this->pattern, \var_export($pregResult, true));
+        }
+        return null;
     }
 }
