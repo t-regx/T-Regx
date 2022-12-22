41a443c673c40f3690b71f6de4209d76c3aed321
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-12-22 04:55:20 +0100

    Add performance optimisation in CommentConsumer of in-place string concatenation

--- src/CleanRegex/Internal/Prepared/Parser/Consumer/CommentConsumer.php
+++ src/CleanRegex/Internal/Prepared/Parser/Consumer/CommentConsumer.php
@@ -5,7 +5,6 @@ use TRegx\CleanRegex\Internal\Prepared\Parser\Convention;
 use TRegx\CleanRegex\Internal\Prepared\Parser\Entity\Comment;
 use TRegx\CleanRegex\Internal\Prepared\Parser\EntitySequence;
 use TRegx\CleanRegex\Internal\Prepared\Parser\Feed\Feed;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Feed\StringConditions;
 
 class CommentConsumer implements Consumer
 {
@@ -24,21 +23,18 @@ class CommentConsumer implements Consumer
 
     public function consume(Feed $feed, EntitySequence $entities): void
     {
-        $strings = new StringConditions();
-        $this->commitToStrings($feed, $strings);
-        $entities->append(new Comment($strings->asString()));
-    }
-
-    private function commitToStrings(Feed $feed, StringConditions $strings): void
-    {
+        $strings = '';
         $commentEnd = $feed->oneOf($this->convention->lineEndings());
         while (!$feed->empty()) {
-            if (!$commentEnd->consumable()) {
-                $strings->add($feed->letter());
-            } else {
-                $strings->add($commentEnd);
+            if ($commentEnd->consumable()) {
+                $strings .= $commentEnd->asString();
+                $commentEnd->commit();
                 break;
             }
+            $letter = $feed->letter();
+            $strings .= $letter->asString();
+            $letter->commit();
         }
+        $entities->append(new Comment($strings));
     }
 }
