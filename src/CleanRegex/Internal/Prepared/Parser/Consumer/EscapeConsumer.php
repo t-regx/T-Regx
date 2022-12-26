4da30334c620e2cd7548660ce816b55d9e808da5
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-12-26 13:03:29 +0100

    Remove unnecessary TerminatingEscape

--- src/CleanRegex/Internal/Prepared/Parser/Consumer/EscapeConsumer.php
+++ src/CleanRegex/Internal/Prepared/Parser/Consumer/EscapeConsumer.php
@@ -1,9 +1,9 @@
 <?php
 namespace TRegx\CleanRegex\Internal\Prepared\Parser\Consumer;
 
+use TRegx\CleanRegex\Internal\Delimiter\TrailingBackslashException;
 use TRegx\CleanRegex\Internal\Prepared\Parser\Entity\Entity;
 use TRegx\CleanRegex\Internal\Prepared\Parser\Entity\Escaped;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Entity\TerminatingEscape;
 use TRegx\CleanRegex\Internal\Prepared\Parser\EntitySequence;
 use TRegx\CleanRegex\Internal\Prepared\Parser\Feed\Feed;
 
@@ -27,6 +27,6 @@ class EscapeConsumer implements Consumer
             $letter->commit();
             return new Escaped($letterString);
         }
-        return new TerminatingEscape();
+        throw new TrailingBackslashException();
     }
 }
