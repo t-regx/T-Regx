c8879d24500a9f407f055d05dee338c6207422d3
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2022-12-26 16:15:52 +0100

    Clean EscapeConsumer

--- src/CleanRegex/Internal/Prepared/Parser/Consumer/EscapeConsumer.php
+++ src/CleanRegex/Internal/Prepared/Parser/Consumer/EscapeConsumer.php
@@ -2,7 +2,6 @@
 namespace TRegx\CleanRegex\Internal\Prepared\Parser\Consumer;
 
 use TRegx\CleanRegex\Internal\Delimiter\TrailingBackslashException;
-use TRegx\CleanRegex\Internal\Prepared\Parser\Entity\Entity;
 use TRegx\CleanRegex\Internal\Prepared\Parser\Entity\Escaped;
 use TRegx\CleanRegex\Internal\Prepared\Parser\EntitySequence;
 use TRegx\CleanRegex\Internal\Prepared\Parser\Feed\Feed;
@@ -16,16 +15,11 @@ class EscapeConsumer implements Consumer
 
     public function consume(Feed $feed, EntitySequence $entities): void
     {
-        $entities->append($this->consumeEscaped($feed));
-    }
-
-    private function consumeEscaped(Feed $feed): Entity
-    {
         if ($feed->empty()) {
             throw new TrailingBackslashException();
         }
         $letterString = $feed->firstLetter();
         $feed->shiftSingle();
-        return new Escaped($letterString);
+        $entities->append(new Escaped($letterString));
     }
 }
