7bf776a65c91c1d0a0c9fd62425fc64b1b529686
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-08-09 04:46:19 +0200

    Add malformed syntax message to SyntaxException

--- test/Unit/_syntaxError.php
+++ test/Unit/_syntaxError.php
@@ -12,14 +12,29 @@ class _syntaxError extends TestCase
     /**
      * @dataProvider malformedPatterns
      */
-    public function test(string $pattern)
+    public function test(string $pattern, string $expectedMessage)
     {
         catching(fn() => new Pattern($pattern))
-            ->assertException(SyntaxException::class);
+            ->assertException(SyntaxException::class)
+            ->assertMessage($expectedMessage);
+    }
+
+    protected function tearDown(): void
+    {
+        \error_clear_last();
     }
 
     public function malformedPatterns(): DataProvider
     {
-        return DataProvider::list(')', '+', '(?<>)', '[[:invalid:]]');
+        return DataProvider::tuples(
+            [')', 'Unmatched closing parenthesis at offset 0.'],
+            ['+', 'Quantifier does not follow a repeatable item at offset 0.'],
+            ['[z-a]', 'Range out of order in character class at offset 3.'],
+            ['(?<>)', 'Subpattern name expected at offset 3.'],
+            ['(?<123>)', 'Subpattern name must start with a non-digit at offset 3.'],
+            ['[[:invalid:]]', 'Unknown POSIX class name at offset 3.'],
+            ['(?<=a+)b', 'Lookbehind assertion is not fixed length at offset 0.'],
+            ['[\Q]', 'Missing terminating ] for character class at offset 4.'],
+        );
     }
 }
