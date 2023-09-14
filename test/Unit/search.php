890717ded41819bc28a99fade913f969d88d6a13
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-09-14 21:23:17 +0200

    Add Pattern.search(string):string[]

---
+++ test/Unit/search.php
@@ -0,0 +1,25 @@
+<?php
+namespace Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Regex\Pattern;
+
+class search extends TestCase
+{
+    public function test()
+    {
+        $pattern = new Pattern('[RB][a-z]+');
+        $this->assertSame(
+            ['Robert', 'Baratheon'],
+            $pattern->search('King Robert I Baratheon was the seventeenth ruler of the Seven Kingdoms and the first king of the dynasty.'));
+    }
+
+    /**
+     * @test
+     */
+    public function unmatched()
+    {
+        $pattern = new Pattern('Valar morghulis');
+        $this->assertEmpty($pattern->search('Dohaeris'));
+    }
+}
