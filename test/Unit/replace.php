a4e4b99b6ca40a995fb2224b504d65df60828e8a
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-09-14 21:29:33 +0200

    Add Pattern.replace(string,string):string

---
+++ test/Unit/replace.php
@@ -0,0 +1,47 @@
+<?php
+namespace Test\Unit;
+
+use PHPUnit\Framework\TestCase;
+use Regex\Pattern;
+
+class replace extends TestCase
+{
+    public function test()
+    {
+        $pattern = new Pattern('do|not');
+        $this->assertSame('We . . sow', $pattern->replace('We do not sow', '.'));
+    }
+
+    /**
+     * @test
+     */
+    public function referenceBackslash()
+    {
+        $pattern = new Pattern('\w+ (\w+)');
+        $this->assertSame(
+            'Mr \1',
+            $pattern->replace('Aemon Targaryen', 'Mr \1'));
+    }
+
+    /**
+     * @test
+     */
+    public function referenceDollar()
+    {
+        $pattern = new Pattern('\w+ (\w+)');
+        $this->assertSame(
+            'Mr $1',
+            $pattern->replace('Aemon Targaryen', 'Mr $1'));
+    }
+
+    /**
+     * @test
+     */
+    public function referenceDollarCurly()
+    {
+        $pattern = new Pattern('\w+ (\w+)');
+        $this->assertSame(
+            'Mr ${1}',
+            $pattern->replace('Aemon Targaryen', 'Mr ${1}'));
+    }
+}
