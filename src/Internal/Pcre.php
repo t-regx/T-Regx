16f7320347cacfdd64f12b1bed4cba5304967863
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2023-12-26 13:18:13 +0100

    Extract Pcre

---
+++ src/Internal/Pcre.php
@@ -0,0 +1,81 @@
+<?php
+namespace Regex\Internal;
+
+use Regex\BacktrackException;
+use Regex\JitException;
+use Regex\RecursionException;
+use Regex\UnicodeException;
+
+class Pcre
+{
+    private DelimitedExpression $expression;
+
+    public function __construct(DelimitedExpression $expression)
+    {
+        $this->expression = $expression;
+    }
+
+    public function test(string $subject): bool
+    {
+        $result = \preg_match($this->expression->delimited, $subject);
+        $this->throwMatchException();
+        return $result;
+    }
+
+    public function count(string $subject): int
+    {
+        $result = \preg_match_all($this->expression->delimited, $subject);
+        $this->throwMatchException();
+        return $result;
+    }
+
+    public function search(string $subject): array
+    {
+        \preg_match_all($this->expression->delimited, $subject, $matches);
+        $this->throwMatchException();
+        return $matches[0];
+    }
+
+    public function replace(string $subject, string $replacement): string
+    {
+        $result = \preg_replace($this->expression->delimited,
+            \str_replace(['\\', '$'], ['\\\\', '\$'], $replacement),
+            $subject);
+        $this->throwMatchException();
+        return $result;
+    }
+
+    public function split(string $subject, int $limit): array
+    {
+        $elements = [];
+        foreach ($this->splitOffsetCapture($subject, $limit) as [$piece, $offset]) {
+            $elements[] = $offset === -1 ? null : $piece;
+        }
+        return $elements;
+    }
+
+    private function splitOffsetCapture(string $subject, int $limit): array
+    {
+        $pieces = \preg_split($this->expression->delimited, $subject, $limit,
+            \PREG_SPLIT_DELIM_CAPTURE | \PREG_SPLIT_OFFSET_CAPTURE);
+        $this->throwMatchException();
+        return $pieces;
+    }
+
+    private function throwMatchException(): void
+    {
+        $error = \preg_last_error();
+        if ($error === \PREG_BACKTRACK_LIMIT_ERROR) {
+            throw new BacktrackException();
+        }
+        if ($error === \PREG_RECURSION_LIMIT_ERROR) {
+            throw new RecursionException();
+        }
+        if ($error === \PREG_BAD_UTF8_ERROR) {
+            throw new UnicodeException('Malformed unicode subject.');
+        }
+        if ($error === \PREG_JIT_STACKLIMIT_ERROR) {
+            throw new JitException();
+        }
+    }
+}
--- src/Pattern.php
+++ src/Pattern.php
@@ -2,6 +2,7 @@
 namespace Regex;
 
 use Regex\Internal\DelimitedExpression;
+use Regex\Internal\Pcre;
 
 final class Pattern
 {
@@ -15,24 +16,22 @@ final class Pattern
     public const DUPLICATE_NAMES = 'J';
 
     private DelimitedExpression $expression;
+    private Pcre $pcre;
 
     public function __construct(string $pattern, string $modifiers = '')
     {
         $this->expression = new DelimitedExpression($pattern, $modifiers);
+        $this->pcre = new Pcre($this->expression);
     }
 
     public function test(string $subject): bool
     {
-        $result = \preg_match($this->expression->delimited, $subject);
-        $this->throwMatchException();
-        return $result;
+        return $this->pcre->test($subject);
     }
 
     public function count(string $subject): int
     {
-        $result = \preg_match_all($this->expression->delimited, $subject);
-        $this->throwMatchException();
-        return $result;
+        return $this->pcre->count($subject);
     }
 
     /**
@@ -40,19 +39,12 @@ final class Pattern
      */
     public function search(string $subject): array
     {
-        \preg_match_all($this->expression->delimited, $subject, $matches);
-        $this->throwMatchException();
-        return $matches[0];
+        return $this->pcre->search($subject);
     }
 
     public function replace(string $subject, string $replacement): string
     {
-        $result = \preg_replace(
-            $this->expression->delimited,
-            \str_replace(['\\', '$'], ['\\\\', '\$'], $replacement),
-            $subject);
-        $this->throwMatchException();
-        return $result;
+        return $this->pcre->replace($subject, $replacement);
     }
 
     /**
@@ -61,38 +53,9 @@ final class Pattern
     public function split(string $subject, int $maxSplits = -1): array
     {
         if ($maxSplits < 0) {
-            return $this->splitSubject($subject, -1);
+            return $this->pcre->split($subject, -1);
         }
-        return $this->splitSubject($subject, $maxSplits + 1);
-    }
-
-    private function splitSubject(string $subject, int $limit): array
-    {
-        $pieces = \preg_split($this->expression->delimited, $subject, $limit,
-            \PREG_SPLIT_DELIM_CAPTURE | \PREG_SPLIT_OFFSET_CAPTURE);
-        $this->throwMatchException();
-        $result = [];
-        foreach ($pieces as [$piece, $offset]) {
-            $result[] = $offset === -1 ? null : $piece;
-        }
-        return $result;
-    }
-
-    private function throwMatchException(): void
-    {
-        $error = \preg_last_error();
-        if ($error === \PREG_BACKTRACK_LIMIT_ERROR) {
-            throw new BacktrackException();
-        }
-        if ($error === \PREG_RECURSION_LIMIT_ERROR) {
-            throw new RecursionException();
-        }
-        if ($error === \PREG_BAD_UTF8_ERROR) {
-            throw new UnicodeException('Malformed unicode subject.');
-        }
-        if ($error === \PREG_JIT_STACKLIMIT_ERROR) {
-            throw new JitException();
-        }
+        return $this->pcre->split($subject, $maxSplits + 1);
     }
 
     public function __toString(): string
