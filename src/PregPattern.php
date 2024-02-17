a8668b380da922f144d737be647508d118c1cd73
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-02-17 14:37:32 +0100

    Add PregPattern.test(), .search() and .delimited()

---
+++ src/PregPattern.php
@@ -0,0 +1,42 @@
+<?php
+namespace Regex;
+
+use Regex\Internal\ExceptionFactory;
+use Regex\Internal\Modifiers;
+use Regex\Internal\OptionSettingExpression;
+use Regex\Internal\ParsedPattern;
+
+final class PregPattern
+{
+    private string $pattern;
+
+    public function __construct(string $pattern)
+    {
+        $parsed = new ParsedPattern($pattern);
+        if ($parsed->errorMessage) {
+            $factory = new ExceptionFactory($pattern, 
+                new OptionSettingExpression('', new Modifiers('')));
+            throw $factory->exceptionFor($parsed->errorMessage);
+        }
+        $this->pattern = $pattern;
+    }
+
+    public function test(string $subject): bool
+    {
+        return \preg_match($this->pattern, $subject);
+    }
+
+    /**
+     * @return string[]
+     */
+    public function search(string $subject): array
+    {
+        \preg_match_all($this->pattern, $subject, $matches);
+        return $matches[0];
+    }
+
+    public function delimited(): string
+    {
+        return $this->pattern;
+    }
+}
