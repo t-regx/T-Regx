6884712d3b129475d23ad2f52b460365aa96eafd
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-05-06 20:29:01 +0200

    Detail.offset() throws UnicodeException for malformed unicode #112

--- test/Unit/Detail/offset.php
+++ test/Unit/Detail/offset.php
@@ -2,7 +2,10 @@
 namespace Test\Unit\Detail;
 
 use PHPUnit\Framework\TestCase;
+use Regex\Detail;
 use Regex\Pattern;
+use Regex\UnicodeException;
+use function Test\Fixture\Functions\catching;
 
 class offset extends TestCase
 {
@@ -11,5 +14,22 @@ class offset extends TestCase
         $pattern = new Pattern('F.r.', 'u');
         $detail = $pattern->first('Óurs is the Füry!');
         $this->assertSame(12, $detail->offset());
+    }
+
+    /**
+     * @test
+     */
+    public function malformedUnicode()
+    {
+        $detail = $this->detail('.w', '€w');
+        catching(fn() => $detail->offset())
+            ->assertException(UnicodeException::class)
+            ->assertMessage('Byte offset 2 does not point to a valid unicode code point.');
+    }
+
+    private function detail(string $pattern, string $subject, string $modifiers = ''): Detail
+    {
+        $unicode = new Pattern($pattern, $modifiers);
+        return $unicode->first($subject);
     }
 }
